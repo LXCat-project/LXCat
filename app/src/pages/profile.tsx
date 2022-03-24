c9d7c2741ae60e5a7d66b090fee7c13e2242763e
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-03-24 11:46:27 +0100

    Prettify profile page

--- app/src/pages/profile.tsx
+++ app/src/pages/profile.tsx
@@ -1,4 +1,5 @@
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from 'next/image'
 import { Layout } from "../shared/Layout";
 
 const ProfilePage = () => {
@@ -10,9 +11,18 @@ const ProfilePage = () => {
     <Layout>
       <h1>Profile</h1>
 
-      <pre>
-        {JSON.stringify(session, undefined, 4)}
-      </pre>
+      <div style={{ display: 'flex', gap: '2rem' }}>
+        <Image src={session.user!.image!} title={`Logged in ${session.user?.name}`} alt="Picture of logged in user" width={80} height={80} />
+        <div>
+          <div>Name</div>
+          <div>{session.user!.name}</div>
+        </div>
+        <div>
+          <div>Email</div>
+          <div>{session.user!.email}</div>
+        </div>
+      </div>
+
       <button onClick={() => signOut()}>Sign out</button>
     </Layout>
   )
