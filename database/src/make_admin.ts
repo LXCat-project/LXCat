37dd558092c6efa297eea14d0130f246ab074ebf
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-06-02 13:53:01 +0200

    Make admin command sets all roles

--- database/src/make_admin.ts
+++ database/src/make_admin.ts
@@ -1,18 +1,20 @@
 import 'dotenv/config'
 import { aql } from "arangojs";
 import { db } from '../../app/src/db'
+import { Role } from '../../app/src/auth/schema'
 
 (async () => {
     try {
         const email = process.argv[2]
-        const role = 'admin'
+        const roles = Role.options
         db.query(aql`
             FOR u IN users
                 FILTER u.email == ${email}
             UPDATE u WITH {
-                roles: PUSH(u.roles, ${role}, true)
+                roles: ${roles}
             } IN users
         `)
+        console.log(`${email} now all roles, including admin`)
     } catch (err) {
         console.error(err);
     }
