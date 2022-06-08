bf4a44fa36a212f5dd13f62e0f0dbdbdbd0d5429
Daan Boer <daan-boer@hotmail.com>
2022-06-08 13:06:34 +0200

    Started with turborepo

--- app/next.config.js
+++ app/next.config.js
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputStandalone: true,
-  },
+  // experimental: {
+  //   outputStandalone: true,
+  // },
   images: {
-    domains: ['s.gravatar.com', 'secure.gravatar.com'],
+    domains: ["s.gravatar.com", "secure.gravatar.com"],
   },
-}
+};
 
 // For local https disable cert checking
-if (process.env.NODE_ENV === 'development' && process.env.NEXTAUTH_URL.search(/^https:\/\/localhost/) > -1) {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+if (
+  process.env.NODE_ENV === "development" &&
+  process.env.NEXTAUTH_URL.search(/^https:\/\/localhost/) > -1
+) {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
