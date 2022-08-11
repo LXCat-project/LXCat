3624c86eed3bd604dfcd170cf8ea6d08657c8d37
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-08-11 11:52:58 +0200

    Format it

--- schema/src/core/parse.test.ts
+++ schema/src/core/parse.test.ts
@@ -1,16 +1,16 @@
 import { describe, expect, it } from "vitest";
 import { parse_charge } from "./parse";
 
-describe('parse_charge()', () => {
-    const testCases: Array<[number, string]> = [
-        [0, ''],
-        [1, '^+'],
-        [-1, '^-'],
-        [2, '^2+'],
-        [-2, '^2-'],
-    ]
-    it.each(testCases)('given %d should render %s', (input, expected) => {
-        const result = parse_charge(input)
-        expect(result).toEqual(expected);
-    })
-})
+describe("parse_charge()", () => {
+  const testCases: Array<[number, string]> = [
+    [0, ""],
+    [1, "^+"],
+    [-1, "^-"],
+    [2, "^2+"],
+    [-2, "^2-"],
+  ];
+  it.each(testCases)("given %d should render %s", (input, expected) => {
+    const result = parse_charge(input);
+    expect(result).toEqual(expected);
+  });
+});
