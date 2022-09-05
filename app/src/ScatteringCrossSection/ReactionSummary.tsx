7426c5f042bd17e5d4fdf1f257804f0e151af72d
Daan Boer <daan-boer@hotmail.com>
2022-09-05 14:40:20 +0200

    Render reaction summary as latex

--- app/src/ScatteringCrossSection/ReactionSummary.tsx
+++ app/src/ScatteringCrossSection/ReactionSummary.tsx
@@ -1,6 +1,10 @@
 import { State } from "@lxcat/database/dist/shared/types/collections";
 import { Reaction, ReactionEntry } from "@lxcat/schema/dist/core/reaction";
 
+import "katex/dist/katex.min.css";
+// @ts-ignore
+import { InlineMath } from "react-katex";
+
 function stateEntry(entry: ReactionEntry<State>) {
   if (entry.count === 1) {
     return entry.state.id;
@@ -10,13 +14,13 @@ function stateEntry(entry: ReactionEntry<State>) {
 
 export const ReactionSummary = (props: Reaction<State>) => {
   const label = reactionLabel(props);
-  return <div>{label}</div>;
+  return <InlineMath>{label}</InlineMath>;
 };
 
 export function reactionLabel(props: Reaction<State>) {
   const lhs = props.lhs.map(stateEntry).join(" + ");
   const rhs = props.rhs.map(stateEntry).join(" + ");
-  const arrow = props.reversible ? "⇋" : "➙";
-  const label = `${lhs} ${arrow} ${rhs}`;
-  return label;
+  const arrow = props.reversible ? "\\leftrightarrow" : "\\rightarrow";
+
+  return `${lhs} ${arrow} ${rhs}`;
 }
