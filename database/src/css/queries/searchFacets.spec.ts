1a46b3989d563e34d525c69768b5001f377bf3d4
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-09-20 10:21:53 +0200

    Create test for selecting multiple tags on cross section set search page
    It should list sets that have at least one cross section which has one of the selected reaction tags.
    
    Refs #33

--- database/src/css/queries/searchFacets.spec.ts
+++ database/src/css/queries/searchFacets.spec.ts
@@ -107,6 +107,59 @@ describe("searchFacets()", () => {
       });
     });
 
+    describe("with tag=Effective or Ionization", () => {
+      let facets: FilterOptions;
+      beforeAll(async () => {
+        const selection: FilterOptions = {
+          ...emptySelection,
+          tag: [ReactionTypeTag.Effective, ReactionTypeTag.Ionization],
+        };
+        facets = await searchFacets(selection);
+      });
+
+      it("should have 2 contributors", () => {
+        const expected = ["Some organization", "Some other organization"];
+        expect(facets.contributor).toEqual(expected);
+      });
+
+      it("should have Effective and Ionization reaction type tags", () => {
+        const expected = ["Effective", "Ionization"];
+        expect(facets.tag).toEqual(expected);
+      });
+
+      it("should have H2, N2, Ar, Arp for state", () => {
+        const expected: StateChoices = {
+          particle: {
+            H2: {
+              charge: {
+                0: {
+                  electronic: {},
+                },
+              },
+            },
+            N2: {
+              charge: {
+                0: {
+                  electronic: {},
+                },
+              },
+            },
+            Ar: {
+              charge: {
+                0: {
+                  electronic: {},
+                },
+                1: {
+                  electronic: {},
+                },
+              },
+            },
+          },
+        };
+        expect(facets.state).toEqual(expected);
+      });
+    });
+
     describe("with org=other", () => {
       let facets: FilterOptions;
       beforeAll(async () => {
