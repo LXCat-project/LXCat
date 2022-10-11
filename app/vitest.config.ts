4e07c17abe6ff6f9cbc9c49d51e6ccfb33683364
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2022-10-11 12:08:51 +0200

    Add unit test support to app
    Also dont repeat unit test docs

---
+++ app/vitest.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitest/config'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    test: {
+        exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', '**/e2e/**']
+    }
+})
