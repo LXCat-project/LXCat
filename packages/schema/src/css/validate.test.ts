f7158dddadf11b3b5fa1b1e7fbb67ed6df1d9304
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2022-11-09 13:44:27 +0100

    schema: incomplete update to tests for validate

--- packages/schema/src/css/validate.test.ts
+++ packages/schema/src/css/validate.test.ts
@@ -82,23 +82,239 @@ describe("validate()", () => {
 
 describe("validate() w/ examples", () => {
   test("no errors", () => {
-    if (validator.validate(data_ok)) {
-      validator.validate_quantum_numbers(data_ok);
-    }
-    console.log(JSON.stringify(validator.errors, null, 2));
+    const isValid = validator.validate(data_ok);
+    expect(isValid).toBeFalsy();
     expect(validator.errors).toHaveLength(0);
   });
 
-  test("w/ errors", () => {
+  test("w/ parity errors", () => {
     // see quantum_number_validator.test.ts for explanation of error counts
-    if (validator.validate(data_parity_nok)) {
-      validator.validate_quantum_numbers(data_parity_nok);
-    }
-    expect(validator.errors).toHaveLength(5);
+    const isValid = validator.validate(data_parity_nok);
+    console.log(JSON.stringify(validator.errors, null, 2));
+    expect(isValid).toBeFalsy();
+    expect(validator.errors).toEqual([]);
+  });
 
-    if (validator.validate(data_momenta_nok)) {
-      validator.validate_quantum_numbers(data_momenta_nok);
-    }
-    expect(validator.errors).toHaveLength(5);
+  test("w/ momenta errors", () => {
+    const isValid = validator.validate(data_momenta_nok);
+    console.log(JSON.stringify(validator.errors, null, 2));
+    expect(isValid).toBeFalsy();
+    const expected = [
+      {
+        "keyword": "LS coupling",
+        "instancePath": "second/electronic/0/config/core",
+        "schemaPath": "",
+        "params": {
+          "scheme": "LS",
+          "config": [
+            {
+              "n": 3,
+              "l": 1,
+              "occupance": 5
+            }
+          ],
+          "term": {
+            "S": 0.5,
+            "L": 1,
+            "P": -1,
+            "J": 2
+          },
+          "allowed": {
+            "J": [
+              1.5,
+              0.5
+            ]
+          }
+        },
+        "message": "term inconsistent: with L1=1, S1=0.5, J1 should be one of 1.5,0.5"
+      },
+      {
+        "keyword": "J1L2 coupling",
+        "instancePath": "third/electronic/0",
+        "schemaPath": "",
+        "params": {
+          "scheme": "J1L2",
+          "config": {
+            "core": {
+              "scheme": "LS",
+              "config": [
+                {
+                  "n": 3,
+                  "l": 1,
+                  "occupance": 5
+                }
+              ],
+              "term": {
+                "S": 0.5,
+                "L": 1,
+                "P": -1,
+                "J": 1.5
+              }
+            },
+            "excited": {
+              "scheme": "LS",
+              "config": [
+                {
+                  "n": 3,
+                  "l": 2,
+                  "occupance": 1
+                }
+              ],
+              "term": {
+                "S": 0.5,
+                "L": 2,
+                "P": 1
+              }
+            }
+          },
+          "term": {
+            "S": 0.5,
+            "K": 1.5,
+            "P": -1,
+            "J": 2.5
+          },
+          "allowed": {
+            "J": [
+              2,
+              1
+            ]
+          }
+        },
+        "message": "term inconsistent: with K=1.5, S2=0.5, J should be one of 2,1"
+      },
+      {
+        "keyword": "J1L2 coupling",
+        "instancePath": "third/electronic/1",
+        "schemaPath": "",
+        "params": {
+          "scheme": "J1L2",
+          "config": {
+            "core": {
+              "scheme": "LS",
+              "config": [
+                {
+                  "n": 3,
+                  "l": 1,
+                  "occupance": 5
+                }
+              ],
+              "term": {
+                "S": 0.5,
+                "L": 1,
+                "P": -1,
+                "J": 1.5
+              }
+            },
+            "excited": {
+              "scheme": "LS",
+              "config": [
+                {
+                  "n": 3,
+                  "l": 2,
+                  "occupance": 1
+                }
+              ],
+              "term": {
+                "S": 0.5,
+                "L": 2,
+                "P": 1
+              }
+            }
+          },
+          "term": {
+            "S": 0.5,
+            "K": 2,
+            "P": -1,
+            "J": 3
+          },
+          "allowed": {
+            "K": [
+              3.5,
+              2.5,
+              1.5,
+              0.5
+            ]
+          }
+        },
+        "message": "term inconsistent: with J1=1.5, L2=2, K should be one of 3.5,2.5,1.5,0.5"
+      },
+      {
+        "keyword": "LS coupling",
+        "instancePath": "phosphorus/electronic/0/config/core",
+        "schemaPath": "",
+        "params": {
+          "scheme": "LS",
+          "config": [
+            {
+              "n": 3,
+              "l": 2,
+              "occupance": 1
+            }
+          ],
+          "term": {
+            "L": 1,
+            "S": 0.5,
+            "P": -1
+          },
+          "allowed": {
+            "P": 1
+          }
+        },
+        "message": "term incosistent with config: for [{n:3,l:2,occupance:1}], parity should be 1"
+      },
+      {
+        "keyword": "LS1 coupling",
+        "instancePath": "phosphorus/electronic/0/config/core",
+        "schemaPath": "",
+        "params": {
+          "scheme": "LS1",
+          "config": {
+            "core": {
+              "scheme": "LS",
+              "config": [
+                {
+                  "n": 3,
+                  "l": 2,
+                  "occupance": 1
+                }
+              ],
+              "term": {
+                "L": 1,
+                "S": 0.5,
+                "P": -1
+              }
+            },
+            "excited": {
+              "scheme": "LS",
+              "config": [
+                {
+                  "n": 4,
+                  "l": 3,
+                  "occupance": 1
+                }
+              ],
+              "term": {
+                "L": 3,
+                "S": 0.5,
+                "P": -1
+              }
+            }
+          },
+          "term": {
+            "L": 3,
+            "S": 0.5,
+            "K": 2.5,
+            "P": 1,
+            "J": 3
+          },
+          "allowed": {
+            "L": 2,
+            "S": 0.5
+          }
+        },
+        "message": "term inconsistent with config: for [{n:3,l:2,occupance:1}], L should be one of 2, and S=0.5"
+      }
+    ];
+    expect(validator.errors).toEqual(expected);
   });
 });
