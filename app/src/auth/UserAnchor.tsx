f2dba192fd54beeaae85af30bcec396e072e41da
Daan Boer <daan-boer@hotmail.com>
2023-02-20 17:55:31 +0100

    Update styling of navigation bar
    Use mantine components where possible.

--- app/src/auth/UserAnchor.tsx
+++ app/src/auth/UserAnchor.tsx
@@ -2,18 +2,26 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
 import Link from "next/link";
 import Image from "next/image";
+import { Button, createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  portrait: { borderRadius: theme.radius.sm },
+}));
 
 export function UserAnchor() {
   const { data: session } = useSession();
+  const { classes } = useStyles();
+
   if (session) {
     const user = session.user;
     return (
       <>
         <Link href="/profile">
           <Image
+            className={classes.portrait}
             src={user.image!}
             title={`Logged in ${user.name}`}
             alt="Picture of logged in user"
@@ -26,7 +34,9 @@ export function UserAnchor() {
   }
   return (
     <>
-      <button onClick={() => signIn()}>Sign in</button>
+      <Button variant="default" compact onClick={() => signIn()}>
+        Sign in
+      </Button>
     </>
   );
 }
--- app/src/shared/NavBar.tsx
+++ app/src/shared/NavBar.tsx
@@ -2,37 +2,127 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import Image from "next/image";
+import {
+  Container,
+  createStyles,
+  Group,
+  Header,
+  Menu,
+  Text,
+  UnstyledButton,
+} from "@mantine/core";
+import { IconDatabase, IconFileText, IconUsers } from "@tabler/icons";
 import Link from "next/link";
-import { CSSProperties } from "react";
-import logo from "../../public/lxcat.png";
 import { UserAnchor } from "../auth/UserAnchor";
+import { LXCatLogo } from "./Logo";
 
-const navStyle: CSSProperties = {
-  backgroundColor: "#254779",
-  display: "flex",
-  color: "#d1d4da",
-  alignItems: "center",
-  paddingLeft: 24,
-  paddingRight: 24,
-  minHeight: 60,
-  gap: 10,
-};
+const useStyles = createStyles((theme) => ({
+  bar: {
+    backgroundColor: theme.colors.brand[6],
+    alignItems: "center",
+    display: "flex",
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  dropdownItem: {
+    ":hover": {
+      textDecorationLine: "none",
+    },
+  },
+  menuItem: {
+    color: theme.colors.brand[0],
+    backgroundColor: theme.colors.brand[6],
+    padding: "8px 12px",
+    borderRadius: theme.radius.sm,
+    "&:hover": {
+      backgroundColor: theme.fn.darken(theme.colors.brand[6], 0.1),
+      textDecorationLine: "none",
+    },
+  },
+}));
+
+const entries = [
+  {
+    icon: <IconDatabase />,
+    label: "Data center",
+    links: [
+      {
+        link: "/scat-cs",
+        label: "Cross sections",
+      },
+      {
+        link: "/scat-css",
+        label: "Cross section sets",
+      },
+    ],
+  },
+  {
+    icon: <IconUsers />,
+    link: "/team",
+    label: "Team",
+  },
+  {
+    icon: <IconFileText />,
+    link: "/docs/index",
+    label: "Documentation",
+  },
+];
 
 export const NavBar = () => {
-  return (
-    <nav style={navStyle}>
-      <Link href="/">
-        <Image src={logo} alt="Logo of LXCat" />
-      </Link>
-      <div
-        style={{ flexGrow: 1, display: "flex", flexDirection: "row", gap: 10 }}
+  const { classes, theme } = useStyles();
+
+  const barItems = entries.map((entry) => {
+    const menuItems = entry.links?.map((item) => (
+      <Menu.Item
+        component="a"
+        className={classes.dropdownItem}
+        key={item.link}
+        href={item.link}
       >
-        <Link href="/data-center">Data center</Link>
-        <Link href="/team">Team</Link>
-        <Link href="/docs/index">Docs</Link>
-      </div>
+        {item.label}
+      </Menu.Item>
+    ));
+
+    if (menuItems) {
+      return (
+        <Menu key={entry.label} trigger="hover" exitTransitionDuration={0}>
+          <Menu.Target>
+            <UnstyledButton className={classes.menuItem}>
+              <Group spacing="xs">
+                {entry.icon}
+                <Text>{entry.label}</Text>
+              </Group>
+            </UnstyledButton>
+          </Menu.Target>
+          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
+        </Menu>
+      );
+    }
+
+    return (
+      <UnstyledButton
+        key={entry.label}
+        component="a"
+        className={classes.menuItem}
+        href={entry.link}
+      >
+        <Group spacing="xs">
+          {entry.icon}
+          <Text>{entry.label}</Text>
+        </Group>
+      </UnstyledButton>
+    );
+  });
+
+  return (
+    <Header height={60} className={classes.bar}>
+      <Link href="/">
+        <LXCatLogo height={70} width={70} color={theme.colors.brand[0]} />
+      </Link>
+      <Container style={{ display: "flex" }}>
+        <Group spacing={3}>{barItems}</Group>
+      </Container>
       <UserAnchor />
-    </nav>
+    </Header>
   );
 };
