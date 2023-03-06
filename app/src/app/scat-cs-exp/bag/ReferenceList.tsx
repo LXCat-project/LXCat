a0a4d5640d77fc13812b78278cd570a27b627ea8
Daan Boer <daan-boer@hotmail.com>
2023-03-06 12:58:27 +0100

    Set `maxHeight` on scrollable tables instead of a fixed `height`

--- app/src/app/scat-cs-exp/bag/ReferenceList.tsx
+++ app/src/app/scat-cs-exp/bag/ReferenceList.tsx
@@ -4,6 +4,16 @@ import { Reference } from "./Reference";
 import { TableScrollArea } from "./Table";
 import { FormattedReference } from "./types";
 
-export const ReferenceList = ({ references }: { references: Array<FormattedReference> }) => (
-  <TableScrollArea headers={[{label: "References", key: "ref"}]} data={references.map((ref) => ({key: ref.id, ref: <Reference>{ref}</Reference>}))} />
-)
+export const ReferenceList = ({
+  references,
+}: {
+  references: Array<FormattedReference>;
+}) => (
+  <TableScrollArea
+    headers={[{ label: "References", key: "ref" }]}
+    data={references.map((ref) => ({
+      key: ref.id,
+      ref: <Reference>{ref}</Reference>,
+    }))}
+  />
+);
--- app/src/app/scat-cs-exp/bag/Table.tsx
+++ app/src/app/scat-cs-exp/bag/Table.tsx
@@ -1,25 +1,30 @@
 "use client";
 
-import { useState } from 'react';
-import { createStyles, Table, ScrollArea } from '@mantine/core';
+import { Box, createStyles, ScrollArea, Table } from "@mantine/core";
+import { useState } from "react";
 
 const useStyles = createStyles((theme) => ({
   header: {
-    position: 'sticky',
+    position: "sticky",
     top: 0,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-    transition: 'box-shadow 150ms ease',
+    backgroundColor: theme.colorScheme === "dark"
+      ? theme.colors.dark[7]
+      : theme.white,
+    transition: "box-shadow 150ms ease",
 
     zIndex: 1,
 
-    '&::after': {
-      content: '""',
-      position: 'absolute',
+    "&::after": {
+      content: "\"\"",
+      position: "absolute",
       left: 0,
       right: 0,
       bottom: 0,
-      borderBottom: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[2]
-        }`,
+      borderBottom: `1px solid ${
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[3]
+          : theme.colors.gray[2]
+      }`,
     },
   },
 
@@ -39,25 +44,39 @@ export type TableRow = { key: string } & Record<string, React.ReactNode>;
 
 export interface GenericTableProps<Row extends TableRow> {
   data: Array<Row>;
-  headers: Array<{ label: React.ReactNode, key: keyof Row }>;
+  headers: Array<{ label: React.ReactNode; key: keyof Row }>;
 }
 
-export function TableScrollArea<Row extends TableRow>({ data, headers }: GenericTableProps<Row>) {
+export function TableScrollArea<Row extends TableRow>(
+  { data, headers }: GenericTableProps<Row>,
+) {
   const { classes, cx } = useStyles();
   const [scrolled, setScrolled] = useState(false);
 
   const rows = data.map((row) => (
     <tr key={row.key}>
-      {headers.map(({ key }) => <td key={`${String(key)}-${row.key}`}>{row[key]}</td>)}
+      {headers.map(({ key }) => (
+        <td key={`${String(key)}-${row.key}`}>{row[key]}</td>
+      ))}
     </tr>
   ));
 
   return (
-    <ScrollArea className={classes.outer} sx={{ height: 300 }} onScrollPositionChange={({ y }) => setScrolled(y !== 0)}>
+    <ScrollArea
+      className={classes.outer}
+      sx={{
+        ".mantine-ScrollArea-viewport": { maxHeight: 300 },
+        ".mantine-ScrollArea-thumb": { zIndex: 1 },
+      }}
+      onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
+    >
       <Table sx={{ minWidth: 700 }}>
-        <thead className={cx(classes.header, { [classes.scrolled]: scrolled })}>
+        <thead
+          className={cx(classes.header, { [classes.scrolled]: scrolled })}
+        >
           <tr>
-            {headers.map(({ key, label }) => <th key={String(key)}>{label}</th>)}
+            {headers.map(({ key, label }) => <th key={String(key)}>{label}
+            </th>)}
           </tr>
         </thead>
         <tbody>{rows}</tbody>
