19cd80c0c36d49bdfe5bab7ed3e0fd0cd3db9f51
Daan Boer <daan-boer@hotmail.com>
2023-03-20 13:42:55 +0100

    Add missing reuse headers

--- app/src/app/scat-cs/bag/colors.ts
+++ app/src/app/scat-cs/bag/colors.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: LXCat team
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 // Category20 color scheme from https://vega.github.io/vega/docs/schemes/
 export const colorScheme = [
   "#1f77b4",
