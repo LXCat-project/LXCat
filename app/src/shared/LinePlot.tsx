2297311cf7c491e6825ab247df7670408a7e6fe3
Daan Boer <daan-boer@hotmail.com>
2023-04-03 18:02:12 +0200

    Add `LinePlot` component
    This component sets up the plotly boilerplate
    to generate a properly formatted line plot.

---
+++ app/src/shared/LinePlot.tsx
@@ -0,0 +1,89 @@
+// SPDX-FileCopyrightText: LXCat team
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+"use client";
+
+import Plotly from "plotly.js-basic-dist";
+import createPlotlyComponent from "react-plotly.js/factory";
+
+interface LinePlotProps {
+  lines: Array<LineConfig>;
+  xAxis: AxisConfig;
+  yAxis: AxisConfig;
+}
+
+interface AxisConfig {
+  label: string;
+  type: "log" | "linear";
+}
+
+interface LineConfig {
+  x: Array<number>;
+  y: Array<number>;
+  color: string;
+}
+
+const Plot = createPlotlyComponent(Plotly);
+
+export const LinePlot = ({ lines, xAxis, yAxis }: LinePlotProps) => {
+  return (
+    <Plot
+      data={lines.map(line => (
+        {
+          x: line.x,
+          y: line.y,
+          mode: "lines+markers",
+          line: { shape: "linear", width: 3 },
+          marker: { color: line.color, size: 8 },
+          name: "",
+        }
+      ))}
+      config={{
+        toImageButtonOptions: {
+          format: "svg",
+        },
+        typesetMath: true,
+        displayModeBar: true,
+        displaylogo: false,
+      }}
+      layout={{
+        margin: {
+          // l: 60,
+          // b: 30,
+          t: 30,
+          r: 10,
+        },
+        xaxis: {
+          title: xAxis.label,
+          type: xAxis.type,
+          tickformat: ".0e",
+          ticks: "inside",
+          tickmode: "linear",
+          tickwidth: 1,
+          // @ts-ignore
+          minor: { ticks: "inside" },
+          linewidth: 1,
+          showline: true,
+          mirror: "allticks",
+        },
+        width: 700,
+        height: 600,
+        yaxis: {
+          title: yAxis.label,
+          type: yAxis.type,
+          tickformat: ".0e",
+          ticks: "inside",
+          tickmode: "linear",
+          tickwidth: 1,
+          // @ts-ignore
+          minor: { ticks: "inside" },
+          linewidth: 1,
+          showline: true,
+          mirror: "allticks",
+        },
+        showlegend: false,
+      }}
+    />
+  );
+};
