2527ab60641f9a01ecfd106db686c954806b596b
Daan Boer <daan-boer@hotmail.com>
2023-04-08 17:15:17 +0200

    Add `ScientificInput` component
    This component uses and internal string buffer to store the
    actual input. When the buffer is parsable as a valid (scientific)
    number, a float is parsed from the buffer and passe to the parent
    via the `onChange`.
    It also forbids the entry of characters that are generally invalid
    (e.g. arbitrary letters, or a double decimal point).

---
+++ app/src/shared/ScientificInput.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { TextInput, TextInputProps } from "@mantine/core";
+import { useState } from "react";
+
+export type ScientificInputProps =
+  & Omit<TextInputProps, "value" | "onChange">
+  & {
+    value?: number;
+    onChange: (value: number) => void | Promise<void>;
+  };
+
+export const ScientificInput = (
+  { value, onChange, ...rest }: ScientificInputProps,
+) => {
+  const [text, setText] = useState<string>(value?.toString() ?? "");
+
+  return (
+    <TextInput
+      {...rest}
+      value={text}
+      onChange={({ currentTarget: { value } }) => {
+        if (value === "") {
+          setText("");
+          onChange(0);
+        } else if (/^\d*\.?\d*e?[\-\+]?[0-9]*$/.test(value)) {
+          setText(value);
+
+          if (/^\d+(?:\.\d+)?(?:e[\-\+]?[0-9]+)?$/.test(value)) {
+            onChange(parseFloat(value));
+          }
+        }
+      }}
+    />
+  );
+};
