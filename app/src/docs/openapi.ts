f9cbb33b1941131cfe1ab0bd24ac0c67e874c9c3
Stijn van Gils <stijn-28@hotmail.com>
2023-11-07 18:04:06 +0100

    Add missing tsconf compiler option.

--- app/src/docs/openapi.ts
+++ app/src/docs/openapi.ts
@@ -4,7 +4,6 @@ import {
 } from "@asteasolutions/zod-to-openapi";
 import { OpenAPIObjectConfig } from "@asteasolutions/zod-to-openapi/dist/v3.0/openapi-generator";
 import { glob } from "glob";
-import dynamic from "next/dynamic";
 
 let _registry: OpenAPIRegistry | null = null;
 export const registry = () => {
@@ -14,36 +13,14 @@ export const registry = () => {
   return _registry;
 };
 
-// import("../app/api/species/children/openapi.ts");
 const files = await glob("./**/openapi.ts", { cwd: "./src/app/api/" });
-// .then((res) => {
-//   console.log(res);
-//   res.forEach((f, _) => {
-//     // const file = f.replace(/\.[^/.]+$/, "");
-//     import(f);
-//   });
-// });
 
-// console.log(files);
-// files.forEach(async (f, _) => {
-//   // const file = f.replace(/\.[^/.]+$/, "");
-//   // import("../app/api/species/children/openapi.ts");
-//   console.log(f);
-//   const mod = await import(`${f}`);
-//   mod.default();
-//   // require("../app/api/species/children/openapi.ts");
-// });
-
-console.log(files);
-
-const endpoints = files.map((x) => {
+files.map((x) => {
   return x.slice(0, -11);
 }).forEach(async (dir) => {
   const mod = await import(`../app/api/${dir}/openapi.ts`);
   mod.default();
 });
-
-console.log(endpoints);
 
 export function generateOpenAPI() {
   const config: OpenAPIObjectConfig = {
