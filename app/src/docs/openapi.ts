67916f19b183cec05b020d47fcd83e80bf6f2e38
Stijn van Gils <stijn-28@hotmail.com>
2023-11-09 12:03:14 +0100

    Make OpenAPI generator use v3.1.

--- app/src/docs/openapi.ts
+++ app/src/docs/openapi.ts
@@ -1,5 +1,6 @@
 import {
   OpenApiGeneratorV3,
+  OpenApiGeneratorV31,
   OpenAPIRegistry,
 } from "@asteasolutions/zod-to-openapi";
 import { OpenAPIObjectConfig } from "@asteasolutions/zod-to-openapi/dist/v3.0/openapi-generator";
@@ -13,18 +14,18 @@ export const registry = () => {
   return _registry;
 };
 
-const files = await glob("./**/openapi.ts", { cwd: "./src/app/api/" });
+const files = await glob("./**/{*.,}openapi.ts", { cwd: "./src/app/api/" });
 
 files.map((x) => {
-  return x.slice(0, -11);
+  return x.slice(0, -10);
 }).forEach(async (dir) => {
-  const mod = await import(`../app/api/${dir}/openapi.ts`);
+  const mod = await import(`../app/api/${dir}openapi.ts`);
   mod.default();
 });
 
 export function generateOpenAPI() {
   const config: OpenAPIObjectConfig = {
-    openapi: "3.0.0",
+    openapi: "3.1.0",
     info: {
       // TODO: figure out what to do with API versioning.
       version: "1.0.0",
@@ -34,7 +35,7 @@ export function generateOpenAPI() {
     servers: [{ url: "v1" }],
   };
 
-  return new OpenApiGeneratorV3(registry().definitions).generateDocument(
+  return new OpenApiGeneratorV31(registry().definitions).generateDocument(
     config,
   );
 }
