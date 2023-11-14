1fad609c4eea4dfb556d8bb6bb9a4c0efbe994f5
Daan Boer <daan-boer@hotmail.com>
2023-11-14 14:09:09 +0100

    Fix REUSE compliance

--- packages/database/src/cli/setup.ts
+++ packages/database/src/cli/setup.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: LXCat team
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import { LXCatDatabase } from "../lxcat-database.js";
 import { systemDb } from "../systemDb.js";
 
--- packages/database/src/test/index.ts
+++ packages/database/src/test/index.ts
@@ -1,1 +1,5 @@
+// SPDX-FileCopyrightText: LXCat team
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 export { LXCatTestDatabase, startDbContainer } from "../testutils.js";
