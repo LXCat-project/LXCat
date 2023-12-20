b0b094320947a6b22bcb3e65ca7fb762d078ccfc
Stijn van Gils <stijn-28@hotmail.com>
2023-12-20 16:07:38 +0100

    Fix reaction endpoints having access restrictions.

--- app/src/app/api/reactions/reversible/route.ts
+++ app/src/app/api/reactions/reversible/route.ts
@@ -4,15 +4,12 @@
 
 import { db } from "@lxcat/database";
 import { okJsonResponse } from "../../../../shared/api-responses";
-import { hasDeveloperRole, hasSessionOrAPIToken } from "../../middleware/auth";
 import { zodMiddleware } from "../../middleware/zod";
 import { RouteBuilder } from "../../route-builder";
 import { querySchema } from "./schemas";
 
 const router = RouteBuilder
   .init()
-  .use(hasSessionOrAPIToken())
-  .use(hasDeveloperRole())
   .use(zodMiddleware(querySchema))
   .get(async (_, ctx) => {
     const { consumes, produces, setIds, typeTags } = ctx.parsedParams.query;
