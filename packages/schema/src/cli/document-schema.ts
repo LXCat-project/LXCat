cbafd0d6030360dd08026bfedf2f2dc327efb45f
Daan Boer <39552979+daanboer@users.noreply.github.com>
2024-01-09 11:08:04 +0100

    Add a `json:doc` script to generate the `LTPDocument` schema (#686)

---
+++ packages/schema/src/cli/document-schema.ts
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: LXCat team
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { LTPDocumentJSONSchema } from "../json-schema/document.js";
+
+console.log(JSON.stringify(LTPDocumentJSONSchema, null, 2));
