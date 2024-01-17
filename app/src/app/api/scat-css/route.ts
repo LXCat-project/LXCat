81959d29ffef274ad663aab0d8b7046771d31438
Stijn van Gils <stijn-28@hotmail.com>
2024-01-17 14:02:04 +0100

    Fix wrong license header

--- app/src/app/api/scat-css/route.ts
+++ app/src/app/api/scat-css/route.ts
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: LXCat team
 //
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: AGPL-3.0-or-later
 
 import { db } from "@lxcat/database";
 import { FilterOptions, SortOptions } from "@lxcat/database/set";
