2b2f7b49a895277392823acb749456539a5ce8d1
Stijn van Gils <stijn-28@hotmail.com>
2024-01-17 14:00:39 +0100

    Add restrictions to scat-css/[id]/ refstyle param

--- app/src/app/api/scat-css/[id]/schemas.ts
+++ app/src/app/api/scat-css/[id]/schemas.ts
@@ -7,7 +7,8 @@ import { z } from "zod";
 export const querySchema = z.object({
   path: z.object({ id: z.string().describe("Cross section set ID") }),
   query: z.object({
-    refstyle: z.string().describe("Style in which to return references.")
+    refstyle: z.union([z.literal("csl"), z.literal("apa"), z.literal("bibtex")])
+      .describe("Style in which to return references.")
       .optional().default("csl"),
   }),
 });
