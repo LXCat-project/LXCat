e3e0a10c532dccccb0b3c18fd1cbc31fcfc3ef99
Daan Boer <daan-boer@hotmail.com>
2024-01-19 15:53:05 +0100

    Add the `Constant` and `Expression` data types

--- packages/schema/src/common/data-types.ts
+++ packages/schema/src/common/data-types.ts
@@ -2,7 +2,16 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { array, literal, number, object, string, TypeOf } from "zod";
+import {
+  array,
+  literal,
+  number,
+  object,
+  string,
+  TypeOf,
+  ZodIssueCode,
+} from "zod";
+import { UnitValue } from "../process/unit-value.js";
 import { Pair } from "./util.js";
 
 export const LUT = object({
@@ -12,3 +21,24 @@ export const LUT = object({
   values: array(Pair(number())),
 });
 export type LUT = TypeOf<typeof LUT>;
+
+export const Constant = object({
+  type: literal("Constant"),
+}).merge(UnitValue(string(), number()));
+export type Constant = TypeOf<typeof Constant>;
+
+export const Expression = object({
+  type: literal("Expression"),
+  expression: string().min(1),
+  parameters: array(string().min(1)).min(1),
+}).superRefine((value, ctx) => {
+  for (const param of value.parameters) {
+    if (!value.expression.includes(param)) {
+      ctx.addIssue({
+        code: ZodIssueCode.custom,
+        message: `Unused parameter ${param}.`,
+      });
+    }
+  }
+});
+export type Expression = TypeOf<typeof Expression>;
