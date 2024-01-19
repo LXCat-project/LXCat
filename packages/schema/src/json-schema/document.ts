79f75b797ca13b603156ebbbdf4a74e0f7e740ac
Daan Boer <daan-boer@hotmail.com>
2024-01-19 16:01:03 +0100

    Improve generated schemas
    Add more reference schemas.

--- packages/schema/src/json-schema/document.ts
+++ packages/schema/src/json-schema/document.ts
@@ -6,8 +6,11 @@ import { zodToJsonSchema } from "zod-to-json-schema";
 import { CSLData } from "../common/csl/data.js";
 import { CSLDateVariable } from "../common/csl/date-variable.js";
 import { CSLNameVariable } from "../common/csl/name-variable.js";
+import { Constant, Expression, LUT } from "../common/data-types.js";
 import { Key } from "../key.js";
 import { NewLTPDocument } from "../new-document.js";
+import { CrossSectionData } from "../process/cross-section/data-types.js";
+import { RateCoefficientData } from "../process/rate-coefficient/data-types.js";
 import { AnySpecies, Composition, Element } from "../species/index.js";
 import { VersionInfo } from "../version-info.js";
 import { VersionedLTPDocument } from "../versioned-document.js";
@@ -23,6 +26,11 @@ export const VersionedLTPDocumentJSONSchema = zodToJsonSchema(
       CSLDateVariable,
       Element,
       Composition,
+      Constant,
+      LUT,
+      Expression,
+      CrossSectionData,
+      RateCoefficientData,
       AnySpecies,
       ...Object.fromEntries(AnySpecies.optionsMap),
     },
@@ -39,6 +47,11 @@ export const NewLTPDocumentJSONSchema = zodToJsonSchema(NewLTPDocument, {
     CSLDateVariable,
     Element,
     Composition,
+    Constant,
+    LUT,
+    Expression,
+    CrossSectionData,
+    RateCoefficientData,
     AnySpecies,
     ...Object.fromEntries(AnySpecies.optionsMap),
   },
--- packages/schema/src/json-schema/mixture.ts
+++ packages/schema/src/json-schema/mixture.ts
@@ -6,8 +6,12 @@ import { zodToJsonSchema } from "zod-to-json-schema";
 import { CSLData } from "../common/csl/data.js";
 import { CSLDateVariable } from "../common/csl/date-variable.js";
 import { CSLNameVariable } from "../common/csl/name-variable.js";
+import { Constant, Expression, LUT } from "../common/data-types.js";
 import { Key } from "../key.js";
 import { LTPMixture } from "../mixture.js";
+import { CrossSectionData } from "../process/cross-section/data-types.js";
+import { RateCoefficientData } from "../process/rate-coefficient/data-types.js";
+import { SetHeader } from "../set-header.js";
 import { AtomComposition } from "../species/composition/atom.js";
 import { HeteronuclearDiatomComposition } from "../species/composition/diatom/heteronuclear.js";
 import { HomonuclearCompositionDescriptor } from "../species/composition/diatom/homonuclear.js";
@@ -26,6 +30,7 @@ export const LTPMixtureJSONSchema = zodToJsonSchema(LTPMixture, {
   definitions: {
     Key,
     VersionInfo,
+    SetHeader,
     CSLData,
     CSLNameVariable,
     CSLDateVariable,
@@ -35,6 +40,11 @@ export const LTPMixtureJSONSchema = zodToJsonSchema(LTPMixture, {
     HeteronuclearDiatomComposition,
     LTICComposition,
     AtomComposition,
+    Constant,
+    LUT,
+    Expression,
+    CrossSectionData,
+    RateCoefficientData,
     AnySpecies,
     ...Object.fromEntries(AnySpecies.optionsMap),
     SummarizedComponent,
