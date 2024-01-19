792ce3921f12afd776f36d7704f752bfedaf6931
Daan Boer <daan-boer@hotmail.com>
2024-01-19 16:01:03 +0100

    Improve generated schemas
    Add more reference schemas.

--- packages/schema/src/json-schema/document.ts
+++ packages/schema/src/json-schema/document.ts
@@ -6,7 +6,10 @@ import { zodToJsonSchema } from "zod-to-json-schema";
 import { CSLData } from "../common/csl/data.js";
 import { CSLDateVariable } from "../common/csl/date-variable.js";
 import { CSLNameVariable } from "../common/csl/name-variable.js";
+import { Constant, Expression, LUT } from "../common/data-types.js";
 import { LTPDocument } from "../document.js";
+import { CrossSectionData } from "../process/cross-section/data-types.js";
+import { RateCoefficientData } from "../process/rate-coefficient/data-types.js";
 import { AnySpecies } from "../species/index.js";
 
 export const LTPDocumentJSONSchema = zodToJsonSchema(LTPDocument, {
@@ -14,8 +17,13 @@ export const LTPDocumentJSONSchema = zodToJsonSchema(LTPDocument, {
     CSLData,
     CSLNameVariable,
     CSLDateVariable,
+    Constant,
+    LUT,
+    Expression,
+    CrossSectionData,
+    RateCoefficientData,
     AnySpecies,
     ...Object.fromEntries(AnySpecies.optionsMap),
   },
-  $refStrategy: "none",
+  $refStrategy: "root",
 });
--- packages/schema/src/json-schema/mixture.ts
+++ packages/schema/src/json-schema/mixture.ts
@@ -6,14 +6,24 @@ import { zodToJsonSchema } from "zod-to-json-schema";
 import { CSLData } from "../common/csl/data.js";
 import { CSLDateVariable } from "../common/csl/date-variable.js";
 import { CSLNameVariable } from "../common/csl/name-variable.js";
+import { Constant, Expression, LUT } from "../common/data-types.js";
 import { LTPMixture } from "../mixture.js";
+import { CrossSectionData } from "../process/cross-section/data-types.js";
+import { RateCoefficientData } from "../process/rate-coefficient/data-types.js";
+import { SetHeader } from "../set-header.js";
 import { AnySpecies } from "../species/index.js";
 
 export const LTPMixtureJSONSchema = zodToJsonSchema(LTPMixture, {
   definitions: {
+    SetHeader,
     CSLData,
     CSLNameVariable,
     CSLDateVariable,
+    Constant,
+    LUT,
+    Expression,
+    CrossSectionData,
+    RateCoefficientData,
     AnySpecies,
     ...Object.fromEntries(AnySpecies.optionsMap),
   },
