192696bfe81c84a14f16ac5c874498895657c8c3
Daan Boer <daan-boer@hotmail.com>
2024-01-19 16:01:03 +0100

    Improve generated schemas
    Add more reference schemas.

--- packages/schema/src/json-schema/document.ts
+++ packages/schema/src/json-schema/document.ts
@@ -6,7 +6,10 @@ import { zodToJsonSchema } from "zod-to-json-schema";
 import { CSLData } from "../common/csl/data.js";
 import { CSLDateVariable } from "../common/csl/date-variable.js";
 import { CSLNameVariable } from "../common/csl/name-variable.js";
+import { Constant, Expression, LUT } from "../common/data-types.js";
 import { NewLTPDocument } from "../new-document.js";
+import { CrossSectionData } from "../process/cross-section/data-types.js";
+import { RateCoefficientData } from "../process/rate-coefficient/data-types.js";
 import { AnySpecies } from "../species/index.js";
 
 export const LTPDocumentJSONSchema = zodToJsonSchema(NewLTPDocument, {
@@ -14,6 +17,11 @@ export const LTPDocumentJSONSchema = zodToJsonSchema(NewLTPDocument, {
     CSLData,
     CSLNameVariable,
     CSLDateVariable,
+    Constant,
+    LUT,
+    Expression,
+    CrossSectionData,
+    RateCoefficientData,
     AnySpecies,
     ...Object.fromEntries(AnySpecies.optionsMap),
   },
