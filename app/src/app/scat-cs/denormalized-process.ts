d7b14258353a88cbfa70c232afe47d9f21b123db
Daan Boer <daan-boer@hotmail.com>
2024-01-30 10:37:40 +0100

    Update `@lxcat/app to work with the new comment fields

--- app/src/app/scat-cs/denormalized-process.ts
+++ app/src/app/scat-cs/denormalized-process.ts
@@ -5,10 +5,11 @@
 import { SerializedSpecies } from "@lxcat/database/schema";
 import type { SetHeader } from "@lxcat/schema";
 import type { CrossSectionInfo, Reaction } from "@lxcat/schema/process";
+import { ReferenceRef } from "@lxcat/schema/reference";
 
 export type DenormalizedProcess = {
   reaction: Reaction<SerializedSpecies>;
-  info: CrossSectionInfo<string> & {
+  info: CrossSectionInfo<ReferenceRef<string>> & {
     _key: string;
     isPartOf: Array<SetHeader>;
   };
