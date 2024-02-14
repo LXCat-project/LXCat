ad0651f916d957991343c68a70c9ce8171385edd
Daan Boer <daan-boer@hotmail.com>
2024-02-14 11:46:42 +0100

    Add `SpeciesBuilder` component
    This component is used to select the reactants and products
    of a reaction in the contributor set edit form.

--- app/src/app/author/set/[id]/edit/process-tab.tsx
+++ app/src/app/author/set/[id]/edit/process-tab.tsx
@@ -12,12 +12,16 @@ import { ReactionEntry } from "@lxcat/schema/process";
 import { AnySpeciesSerializable } from "@lxcat/schema/species";
 import {
   Accordion,
+  ActionIcon,
+  Button,
   Fieldset,
   Group,
   MultiSelect,
+  NumberInput,
   ScrollArea,
   Stack,
 } from "@mantine/core";
+import { IconTrash } from "@tabler/icons-react";
 import { useMemo } from "react";
 import Latex from "react-latex-next";
 import classes from "./process-tab.module.css";
@@ -84,6 +88,63 @@ const ProcessInfoItem = (
   );
 };
 
+const SpeciesBuilder = (
+  { entries, species, onChange }: {
+    entries: Process["reaction"]["lhs"];
+    species: Record<string, string>;
+    onChange: (entries: Process["reaction"]["lhs"]) => MaybePromise<void>;
+  },
+) => (
+  <Stack>
+    {entries.map((entry, index) => {
+      return (
+        <Group justify="space-between">
+          <NumberInput
+            allowDecimal={false}
+            allowNegative={false}
+            min={1}
+            style={{ width: "70px" }}
+            value={entry.count}
+            onChange={(count) => {
+              const newEntries = [...entries];
+              newEntries[index].count = count as number;
+              return onChange(newEntries);
+            }}
+          />
+          <LatexSelect
+            key={entry.state}
+            value={entry.state}
+            data={species}
+            onChange={(speciesKey) => {
+              const newEntries = [...entries];
+              newEntries[index].state = speciesKey!;
+              return onChange(newEntries);
+            }}
+            grow
+          />
+          <ActionIcon
+            variant="subtle"
+            color="red"
+            onClick={() =>
+              onChange(entries.filter((_, curIndex) => curIndex !== index))}
+          >
+            <IconTrash />
+          </ActionIcon>
+        </Group>
+      );
+    })}
+    <Button
+      onClick={() => {
+        if (entries.every((entry) => entry.state in species)) {
+          onChange([...entries, { count: 1, state: "" }]);
+        }
+      }}
+    >
+      +
+    </Button>
+  </Stack>
+);
+
 const ReactionBuilder = (
   { reaction, species, onChange }: {
     reaction: Process["reaction"];
@@ -91,13 +152,27 @@ const ReactionBuilder = (
     onChange: (reaction: Process["reaction"]) => MaybePromise<void>;
   },
 ) => (
-  <Group>
+  <Group justify="center">
+    <Fieldset legend="Reactants">
+      <SpeciesBuilder
+        entries={reaction.lhs}
+        species={species}
+        onChange={(lhs) => onChange({ ...reaction, lhs })}
+      />
+    </Fieldset>
     <LatexSelect
       value={reaction.reversible ? "true" : "false"}
       data={{ false: "\\rightarrow", true: "\\leftrightarrow" }}
       onChange={(value) =>
         onChange({ ...reaction, reversible: value === "true" })}
     />
+    <Fieldset legend="Products">
+      <SpeciesBuilder
+        entries={reaction.rhs}
+        species={species}
+        onChange={(rhs) => onChange({ ...reaction, rhs })}
+      />
+    </Fieldset>
   </Group>
 );
 
