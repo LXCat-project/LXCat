3c3f8b63bc178c216df17305dd65dcb8c0dc292c
Daan Boer <daan-boer@hotmail.com>
2024-03-19 12:07:27 +0100

    Versioned `WithReference` types should allow for `string` type references
    For preformatted references.

--- packages/schema/src/mixture.ts
+++ packages/schema/src/mixture.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { output, z } from "zod";
+import { output, z, ZodTypeAny } from "zod";
 import { Reference, ReferenceRef } from "./common/reference.js";
 import { Contributor } from "./contributor.js";
 import { ProcessInfo } from "./process/process-info.js";
@@ -13,47 +13,50 @@ import { SetHeader } from "./set-header.js";
 import { SerializedSpecies } from "./species/serialized.js";
 import { versioned } from "./versioned.js";
 
-const MixtureBody = z.object({
-  sets: z.record(versioned(SetHeader(Contributor))),
-  references: z.record(Reference),
-  states: z.record(SerializedSpecies),
-  processes: z.array(
-    Process(
-      z.string(),
-      versioned(
-        ProcessInfo(ReferenceRef(z.string().min(1))).merge(SetReference),
+const MixtureBody = <ReferenceType extends ZodTypeAny>(
+  Reference: ReferenceType,
+) =>
+  z.object({
+    sets: z.record(versioned(SetHeader(Contributor))),
+    references: z.record(Reference),
+    states: z.record(SerializedSpecies),
+    processes: z.array(
+      Process(
+        z.string(),
+        versioned(
+          ProcessInfo(ReferenceRef(z.string().min(1))).merge(SetReference),
+        ),
       ),
     ),
-  ),
-});
+  })
+    .refine(
+      (doc) =>
+        doc.processes
+          .flatMap((process) => process.reaction.lhs)
+          .every(({ state }) => state in doc.states)
+        && doc.processes
+          .flatMap((process) => process.reaction.rhs)
+          .every(({ state }) => state in doc.states),
+      "Referenced state key is missing in states record.",
+    )
+    .refine(
+      (doc) =>
+        doc.processes
+          .flatMap(({ info }) => info)
+          .flatMap(({ references }) => references)
+          .every((reference) =>
+            typeof reference === "string"
+              ? reference in doc.references
+              : reference.id in doc.references
+          ),
+      "Referenced reference key is missing in references record.",
+    );
 
-export const LTPMixture = MixtureBody
-  .refine(
-    (doc) =>
-      doc.processes
-        .flatMap((process) => process.reaction.lhs)
-        .every(({ state }) => state in doc.states)
-      && doc.processes
-        .flatMap((process) => process.reaction.rhs)
-        .every(({ state }) => state in doc.states),
-    "Referenced state key is missing in states record.",
-  )
-  .refine(
-    (doc) =>
-      doc.processes
-        .flatMap(({ info }) => info)
-        .flatMap(({ references }) => references)
-        .every((reference) =>
-          typeof reference === "string"
-            ? reference in doc.references
-            : reference.id in doc.references
-        ),
-    "Referenced reference key is missing in references record.",
-  );
+export const LTPMixture = MixtureBody(Reference);
 export type LTPMixture = output<typeof LTPMixture>;
 
 export const LTPMixtureWithReference = z.intersection(
   SelfReference,
-  LTPMixture,
+  MixtureBody(Reference.or(z.string().min(1))),
 );
 export type LTPMixtureWithReference = output<typeof LTPMixtureWithReference>;
--- packages/schema/src/versioned-document.ts
+++ packages/schema/src/versioned-document.ts
@@ -2,57 +2,60 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { z } from "zod";
+import { z, ZodTypeAny } from "zod";
 import { Reference, ReferenceRef } from "./common/reference.js";
 import { Contributor } from "./contributor.js";
-import { LTPMixture } from "./mixture.js";
 import { VersionedProcess } from "./process/versioned-process.js";
 import { SelfReference } from "./self-reference.js";
 import { SetHeader } from "./set-header.js";
 import { AnySpecies } from "./species/any-species.js";
 import { versioned } from "./versioned.js";
 
-const VersionedDocumentBody = z.object({
-  references: z.record(Reference),
-  states: z.record(AnySpecies),
-  processes: z.array(
-    VersionedProcess(z.string(), ReferenceRef(z.string().min(1))),
-  ),
-});
+const VersionedDocumentBody = <ReferenceType extends ZodTypeAny>(
+  Reference: ReferenceType,
+) =>
+  versioned(
+    SetHeader(Contributor).merge(
+      z.object({
+        references: z.record(Reference),
+        states: z.record(AnySpecies),
+        processes: z.array(
+          VersionedProcess(z.string(), ReferenceRef(z.string().min(1))),
+        ),
+      }),
+    ),
+  )
+    .refine(
+      (doc) =>
+        doc.processes
+          .flatMap((process) => process.reaction.lhs)
+          .every(({ state }) => state in doc.states)
+        && doc.processes
+          .flatMap((process) => process.reaction.rhs)
+          .every(({ state }) => state in doc.states),
+      "Referenced state key is missing in states record.",
+    )
+    .refine(
+      (doc) =>
+        doc.processes
+          .flatMap(({ info }) => info)
+          .flatMap(({ references }) => references)
+          .every((reference) =>
+            typeof reference === "string"
+              ? reference in doc.references
+              : reference.id in doc.references
+          ),
+      "Referenced reference key is missing in references record.",
+    );
 
 // Contains _key and version information. Datasets downloaded from LXCat use
 // this schema.
-export const VersionedLTPDocument = versioned(
-  SetHeader(Contributor).merge(VersionedDocumentBody),
-)
-  .refine(
-    (doc) =>
-      doc.processes
-        .flatMap((process) => process.reaction.lhs)
-        .every(({ state }) => state in doc.states)
-      && doc.processes
-        .flatMap((process) => process.reaction.rhs)
-        .every(({ state }) => state in doc.states),
-    "Referenced state key is missing in states record.",
-  )
-  .refine(
-    (doc) =>
-      doc.processes
-        .flatMap(({ info }) => info)
-        .flatMap(({ references }) => references)
-        .every((reference) =>
-          typeof reference === "string"
-            ? reference in doc.references
-            : reference.id in doc.references
-        ),
-    "Referenced reference key is missing in references record.",
-  );
-
+export const VersionedLTPDocument = VersionedDocumentBody(Reference);
 export type VersionedLTPDocument = z.output<typeof VersionedLTPDocument>;
 
 export const VersionedLTPDocumentWithReference = z.intersection(
   SelfReference,
-  VersionedLTPDocument,
+  VersionedDocumentBody(Reference.or(z.string().min(1))),
 );
 export type VersionedLTPDocumentWithReference = z.output<
   typeof VersionedLTPDocumentWithReference
