081072ac3c4adfd8dd30260fd563880e9460e37d
Daan Boer <daan-boer@hotmail.com>
2024-03-19 12:44:17 +0100

    Fix REUSE compliance

--- packages/schema/src/species/serialized.ts
+++ packages/schema/src/species/serialized.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: LXCat team
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { object, TypeOf } from "zod";
 import { AnySpecies } from "./any-species.js";
 import { StateSummary } from "./summary.js";
