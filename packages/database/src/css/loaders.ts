8b37b0b44324691b45a96028d8c86b9dc1356748
Daan Boer <daan-boer@hotmail.com>
2024-03-24 11:25:32 +0100

    Add `load-orgs` command
    This command is used to load a directory of
    organization definitions.

--- packages/database/src/css/loaders.ts
+++ packages/database/src/css/loaders.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import { NewLTPDocument } from "@lxcat/schema";
+import { Contributor, NewLTPDocument } from "@lxcat/schema";
 import { readdir, readFile } from "fs/promises";
 import { join } from "path";
 import { LXCatDatabase } from "../lxcat-database.js";
@@ -10,8 +10,8 @@ import { LXCatDatabase } from "../lxcat-database.js";
 async function load_css(db: LXCatDatabase, fn: string) {
   const content = await readFile(fn, { encoding: "utf8" });
   const body = NewLTPDocument.parse(JSON.parse(content));
-  const cs_set_id = await db.createSet(body);
-  console.log(`Inserted ${fn} as ${cs_set_id} into CrossSectionSet collection`);
+  const id = await db.createSet(body);
+  console.log(`Inserted ${fn} as ${id} into CrossSectionSet collection`);
 }
 
 export async function load_css_dir(db: LXCatDatabase, dir: string) {
@@ -23,3 +23,22 @@ export async function load_css_dir(db: LXCatDatabase, dir: string) {
     }
   }
 }
+
+const load_organization_from_file = async (db: LXCatDatabase, path: string) => {
+  const content = await readFile(path, { encoding: "utf8" });
+  const org = Contributor.parse(JSON.parse(content));
+  const orgId = await db.addOrganization(org);
+  console.log(`Inserted organization ${org.name} with key ${orgId}.`);
+};
+
+export const load_organizations_dir = async (
+  db: LXCatDatabase,
+  dir: string,
+) => {
+  const files = await readdir(dir);
+  for (const file of files) {
+    if (file.endsWith(".json")) {
+      await load_organization_from_file(db, join(dir, file));
+    }
+  }
+};
