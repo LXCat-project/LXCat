5eb10457bf67dca3708ad9ecdf9f09571dd9024c
Daan Boer <39552979+daanboer@users.noreply.github.com>
2024-03-25 16:18:10 +0000

    Add command to separately load organizations (#890)
    * Add `load-orgs` command
    
    This command is used to load a directory of
    organization definitions.
    
    * Use `load_organizations_dir` during testing
    
    Adds an `organizations` seed directory that contains two
    dummy organization definitions.

--- packages/database/src/test/seed/1_users.ts
+++ packages/database/src/test/seed/1_users.ts
@@ -1,14 +1,16 @@
 // SPDX-FileCopyrightText: LXCat team
 
 // SPDX-License-Identifier: AGPL-3.0-or-later
-import "dotenv/config";
-import { Contributor } from "@lxcat/schema";
-import { Database } from "arangojs";
-import { EdgeCollection } from "arangojs/collection.js";
-import { UserWithAccountSessionInDb } from "../../auth/schema.js";
 
-export default async (db: Database) => {
-  const users = db.collection<UserWithAccountSessionInDb>("users");
+import "dotenv/config";
+import { EdgeCollection } from "arangojs/collection.js";
+import { dirname, join } from "path";
+import { UserWithAccountSessionInDb } from "../../auth/schema.js";
+import { load_organizations_dir } from "../../css/loaders.js";
+import { LXCatTestDatabase } from "../index.js";
+
+export default async (db: LXCatTestDatabase) => {
+  const users = db.getDB().collection<UserWithAccountSessionInDb>("users");
   const user = UserWithAccountSessionInDb.parse({
     name: "somename",
     email: "somename@example.com",
@@ -19,26 +21,26 @@ export default async (db: Database) => {
   // TODO hide logs of seed behind a flag
   console.log(`Test user added with _key = ${newUser._key}`);
 
-  const organizations = db.collection<Contributor>("Organization");
-  const organization: Contributor = {
-    name: "Some organization",
-    description: "Description of some organization.",
-    contact: "info@some-org.com",
-    howToReference: "",
-  };
-  const newOrganization = await organizations.save(organization, {
-    returnNew: true,
-  });
-  console.log(`Test organization added with _key = ${newOrganization._key}`);
+  const thisfile = new URL(import.meta.url);
+  await load_organizations_dir(
+    db,
+    join(dirname(thisfile.pathname), "organizations"),
+  );
 
-  const memberships: EdgeCollection = db.collection("MemberOf");
+  const orgId = await db.getOrganizationByName("Some organization");
+
+  if (orgId === undefined) {
+    throw new Error("Could not find test organization: Some organization.");
+  }
+
+  const memberships: EdgeCollection = db.getDB().collection("MemberOf");
   await memberships.save({
     _from: newUser._id,
-    _to: newOrganization._id,
+    _to: orgId,
   });
-  console.log("Test user member of test organization");
+
   return {
     testUserKey: newUser._key,
-    testOrgKey: newOrganization._key,
+    testOrgKey: orgId.split("/")[1],
   };
 };
