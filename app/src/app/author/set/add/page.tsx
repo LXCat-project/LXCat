c317fda689a9708f44b82c172ffcc6d79929633a
Daan Boer <daan-boer@hotmail.com>
2024-04-04 17:20:12 +0200

    Add the `/author/set/add` page
    Completely reuses the `EditForm` component.

---
+++ app/src/app/author/set/add/page.tsx
@@ -0,0 +1,23 @@
+import { options } from "@/auth/options";
+import { Unauthorized } from "@/shared/unauthorized";
+import { db } from "@lxcat/database";
+import { getServerSession } from "next-auth";
+import { EditForm } from "../[id]/edit/edit-form";
+import { emptySet } from "../empty-set";
+
+const AddSetPage = async () => {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    return <Unauthorized />;
+  }
+
+  return (
+    <EditForm
+      initialSet={emptySet()}
+      organizations={await db().getAffiliations(session.user.email)}
+    />
+  );
+};
+
+export default AddSetPage;
