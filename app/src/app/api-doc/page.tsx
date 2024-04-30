ec5385a68be4d0ceaa4983b7fc193e431ea6fb78
Daan Boer <daan-boer@hotmail.com>
2024-04-30 16:30:45 +0200

    Fix `/api-doc` page in production
    By using a relative path to `/api/doc`.

--- app/src/app/api-doc/page.tsx
+++ app/src/app/api-doc/page.tsx
@@ -6,7 +6,7 @@
 import { RedocStandalone } from "redoc";
 
 export default function IndexPage() {
-  const url = `${process.env.NEXT_PUBLIC_URL}/api/doc/`;
+  const url = "/api/doc/";
   return (
     <section className="container">
       <RedocStandalone specUrl={url} />
