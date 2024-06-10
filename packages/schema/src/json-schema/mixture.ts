3dcbb135ce512444a6ac3a2d2168d71fa898e90e
Daan Boer <daan-boer@hotmail.com>
2024-06-10 23:38:53 +0200

    Update JSON schema generation to include composition types

--- packages/schema/src/json-schema/mixture.ts
+++ packages/schema/src/json-schema/mixture.ts
@@ -8,6 +8,12 @@ import { CSLDateVariable } from "../common/csl/date-variable.js";
 import { CSLNameVariable } from "../common/csl/name-variable.js";
 import { Key } from "../key.js";
 import { LTPMixture } from "../mixture.js";
+import { AtomComposition } from "../species/composition/atom.js";
+import { HeteronuclearDiatomComposition } from "../species/composition/diatom/heteronuclear.js";
+import { HomonuclearCompositionDescriptor } from "../species/composition/diatom/homonuclear.js";
+import { Element } from "../species/composition/element.js";
+import { LTICComposition } from "../species/composition/triatom/ltic.js";
+import { Composition } from "../species/composition/universal.js";
 import {
   AnySpecies,
   SerializedSpecies,
@@ -22,6 +28,12 @@ export const LTPMixtureJSONSchema = zodToJsonSchema(LTPMixture, {
     CSLData,
     CSLNameVariable,
     CSLDateVariable,
+    Element,
+    Composition,
+    HomonuclearCompositionDescriptor,
+    HeteronuclearDiatomComposition,
+    LTICComposition,
+    AtomComposition,
     AnySpecies,
     ...Object.fromEntries(AnySpecies.optionsMap),
     StateSummary,
