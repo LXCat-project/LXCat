dd415b6a4fc7cad271aceb21a6dd8df6d843aef7
Daan Boer <daan-boer@hotmail.com>
2024-06-17 16:23:26 +0200

    Allow recursive definition of composition
    E.g. Si(CH3)4 can now be recursively defined as
    `[['Si', 1], [[['C', 1], ['H', 3']], 4]]`.

--- packages/schema/src/species/composition/universal.ts
+++ packages/schema/src/species/composition/universal.ts
@@ -1,32 +1,44 @@
-import { array, number, tuple, TypeOf } from "zod";
+import { array, lazy, number, tuple, TypeOf, ZodType } from "zod";
 import { parseCharge, parseChargeLatex } from "../common.js";
 import { SummarizedComponent } from "../summary.js";
 import { Element } from "./element.js";
 
-export const Composition = array(
-  tuple([Element, number().int().positive()]),
+// Recursive types cannot be automatically inferred, see:
+// https://zod.dev/?id=recursive-types.
+type Composition = Array<[TypeOf<typeof Element> | Composition, number]>;
+
+export const Composition: ZodType<Composition> = array(
+  tuple([Element.or(lazy(() => Composition)), number().int().positive()]),
 );
-export type Composition = TypeOf<typeof Composition>;
+
+const parseCompositionImpl = (composition: Composition): string =>
+  composition.map(([element, count]) => {
+    const serializedElement = typeof element === "string"
+      ? element
+      : `(${parseCompositionImpl(element)})`;
+    return count === 1 ? serializedElement : `${serializedElement}${count}`;
+  }).join("");
 
 export const parseComposition = (
   composition: Composition,
   charge: number,
-): string =>
-  `${
-    composition.map(([element, count]) =>
-      count === 1 ? `${element}` : `${element}${count}`
-    ).join("")
-  }${parseCharge(charge)}`;
+): string => `${parseCompositionImpl(composition)}${parseCharge(charge)}`;
+
+const parseCompositionLatexImpl = (composition: Composition): string =>
+  composition.map(([element, count]) => {
+    const serializedElement = typeof element === "string"
+      ? element
+      : `\\left(${parseCompositionLatexImpl(element)}\\right)`;
+    return count === 1 ? serializedElement : `${serializedElement}_{${count}}`;
+  }).join("");
 
 export const parseCompositionLatex = (
   composition: Composition,
   charge: number,
 ): string =>
-  `\\mathrm{${
-    composition.map(([element, count]) =>
-      count === 1 ? element : `${element}_{${count}}`
-    ).join("")
-  }}${parseChargeLatex(charge)}`;
+  `\\mathrm{${parseCompositionLatexImpl(composition)}}${
+    parseChargeLatex(charge)
+  }`;
 
 export const compositionSummary = (
   composition: Composition,
