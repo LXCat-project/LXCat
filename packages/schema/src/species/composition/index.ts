31ce3f0125db32e0c97e47aba47f124d55062148
Daan Boer <daan-boer@hotmail.com>
2024-06-17 17:21:58 +0200

    Export composition types from schema package

---
+++ packages/schema/src/species/composition/index.ts
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: LXCat team
+//
+// SPDX-License-Identifier: Apache-2.0
+
+export { Element } from "./element.js";
+export { Composition } from "./universal.js";
--- packages/schema/src/species/index.ts
+++ packages/schema/src/species/index.ts
@@ -3,5 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export * from "./any-species.js";
+export * from "./composition/index.js";
 export { SerializedSpecies } from "./serialized.js";
 export * from "./summary.js";
