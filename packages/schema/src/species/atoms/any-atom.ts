d906141c84bb397f7a38524f7167a64a6c1deb39
Daan Boer <daan-boer@hotmail.com>
2024-06-22 16:14:40 +0200

    Introduce `AtomUnspecified` species type

--- packages/schema/src/species/atoms/any-atom.ts
+++ packages/schema/src/species/atoms/any-atom.ts
@@ -7,9 +7,11 @@ import { Atom } from "./atom.js";
 import { AtomJ1L2 } from "./j1l2.js";
 import { AtomLS } from "./ls.js";
 import { AtomLS1 } from "./ls1.js";
+import { AtomUnspecified } from "./unspecified.js";
 
 export const AnyAtom = discriminatedUnion("type", [
   Atom,
+  AtomUnspecified.plain,
   AtomLS.plain,
   AtomLS1.plain,
   AtomJ1L2.plain,
@@ -18,6 +20,7 @@ export type AnyAtom = output<typeof AnyAtom>;
 
 export const AnyAtomSerializable = discriminatedUnion("type", [
   Atom,
+  AtomUnspecified.serializable,
   AtomLS.serializable,
   AtomLS1.serializable,
   AtomJ1L2.serializable,
