695685303ce057c1f79fe82768ffafdefc2943f4
Daan Boer <daan-boer@hotmail.com>
2024-07-25 17:56:15 +0200

    Add command to generate migration document JSON schema

---
+++ packages/schema/src/cli/migration-schema.ts
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: LXCat team
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { LXCat2MigrationSchema } from "../json-schema/migration-document.js";
+
+console.log(JSON.stringify(LXCat2MigrationSchema, null, 2));
