01591ede18e059f256822e79d4f91ad02010e322
Daan Boer <39552979+daanboer@users.noreply.github.com>
2024-07-26 23:53:16 +0200

    Minor `@lxcat/schema` cleanups (#1213)
    * Fix nonsensical vibrational description in test
    
    * Add additional definitions when generating document schema

--- packages/schema/src/json-schema/document.ts
+++ packages/schema/src/json-schema/document.ts
@@ -6,14 +6,20 @@ import { zodToJsonSchema } from "zod-to-json-schema";
 import { CSLData } from "../common/csl/data.js";
 import { CSLDateVariable } from "../common/csl/date-variable.js";
 import { CSLNameVariable } from "../common/csl/name-variable.js";
-import { NewLTPDocument } from "../new-document.js";
-import { AnySpecies } from "../species/index.js";
+import { Key } from "../key.js";
+import { AnySpecies, Composition, Element } from "../species/index.js";
+import { VersionInfo } from "../version-info.js";
+import { VersionedLTPDocument } from "../versioned-document.js";
 
-export const LTPDocumentJSONSchema = zodToJsonSchema(NewLTPDocument, {
+export const LTPDocumentJSONSchema = zodToJsonSchema(VersionedLTPDocument, {
   definitions: {
+    Key,
+    VersionInfo,
     CSLData,
     CSLNameVariable,
     CSLDateVariable,
+    Element,
+    Composition,
     AnySpecies,
     ...Object.fromEntries(AnySpecies.optionsMap),
   },
