f521549489157e2b58900a3e79ff1bc49288df07
Daan Boer <daan-boer@hotmail.com>
2024-07-31 14:02:54 +0200

    Add `useEffect` hook to call server function
    Add `onChange` callback to communicate search results.

--- app/src/app/cs-set/periodic-table.tsx
+++ app/src/app/cs-set/periodic-table.tsx
@@ -6,23 +6,30 @@
 
 import { Element } from "@lxcat/schema/species";
 import { Grid, GridCol } from "@mantine/core";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { PeriodicSearchResult } from "../../../../packages/database/dist/elements/queries";
+import { MaybePromise } from "../api/util";
 import { getSetHeaderAction } from "./actions";
 import { elements } from "./elements-processed";
 import { PeriodicElement } from "./periodic-element";
 
 export const PeriodicTable = (
-  { activeElements }: { activeElements: Set<Element> },
+  { activeElements, onChange }: {
+    activeElements: Set<Element>;
+    onChange: (setHeaders: Array<PeriodicSearchResult>) => MaybePromise<void>;
+  },
 ) => {
   const [selected, setSelected] = useState<Array<boolean>>(
     elements.map(() => false),
   );
 
-  getSetHeaderAction(
-    elements.filter((_, idx) => selected[idx]).map(({ symbol }) =>
-      Element.parse(symbol)
-    ),
-  ).then((test) => console.log(test));
+  useEffect(() => {
+    getSetHeaderAction(
+      elements.filter((_, idx) => selected[idx]).map(({ symbol }) =>
+        Element.parse(symbol)
+      ),
+    ).then(onChange);
+  }, [selected]);
 
   const disabled = useMemo(() =>
     elements.map((element) => {
@@ -37,7 +44,7 @@ export const PeriodicTable = (
       id="periodic-table"
       columns={18}
       gutter={0}
-      style={{ width: 1080, marginTop: 50 }}
+      style={{ width: 1080 }}
     >
       {elements.map((element, idx) => {
         return (
