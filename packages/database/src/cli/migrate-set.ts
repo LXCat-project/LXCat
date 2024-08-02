babae2ca83c1d6be81da7878bb46dc1bb7cfa370
Daan Boer <daan-boer@hotmail.com>
2024-08-02 18:02:42 +0200

    Use `jsonpatch` to load versions >1

--- packages/database/src/cli/migrate-set.ts
+++ packages/database/src/cli/migrate-set.ts
@@ -4,21 +4,41 @@
 
 import { LXCatMigrationDocument } from "@lxcat/schema/migration";
 import "./env.js";
+import jsonpatch from "jsonpatch";
 import fs from "node:fs/promises";
 import path from "node:path";
 import { exit } from "node:process";
 import { db } from "../db.js";
 
 const fixTimestamps = (dataset: any) => {
-  dataset.versionInfo.createdOn += "Z";
+  if (dataset.versionInfo.createdOn.slice(-1) != "Z") {
+    dataset.versionInfo.createdOn += "Z";
+  }
 
   for (
     const info of dataset.processes.flatMap((process: any) => process.info)
   ) {
-    info.versionInfo.createdOn += "Z";
+    if (info.versionInfo.createdOn.slice(-1) != "Z") {
+      info.versionInfo.createdOn += "Z";
+    }
   }
 
   return dataset;
+};
+
+const patchDocument = (document: LXCatMigrationDocument, patch: any) => {
+  let data = jsonpatch.apply_patch(document, patch);
+  data["processes"] = Object.values(data["processes"]);
+  data = fixTimestamps(data);
+
+  const result = LXCatMigrationDocument.safeParse(data);
+
+  if (!result.success) {
+    console.log(result.error.issues);
+    exit();
+  }
+
+  return result.data;
 };
 
 (async () => {
@@ -26,8 +46,6 @@ const fixTimestamps = (dataset: any) => {
 
   const v1 = path.join(dir, "v1.json");
   console.log(v1);
-
-  await fs.access(v1);
 
   let data = JSON.parse(await fs.readFile(v1, "utf8"));
   data["processes"] = Object.values(data["processes"]);
@@ -41,4 +59,24 @@ const fixTimestamps = (dataset: any) => {
   }
 
   db().loadHistoricDataset(result.data, "Migrated from LXCat 2.");
+
+  let version = 2;
+
+  while (true) {
+    try {
+      const patch = JSON.parse(
+        await fs.readFile(
+          path.join(dir, `v${version}_patch.json`),
+          "utf8",
+        ),
+      );
+      data = patchDocument(data, patch);
+      db().loadHistoricDataset(data, "Migrated from LXCat 2.");
+    } catch (err) {
+      console.log(err);
+      break;
+    }
+
+    version += 1;
+  }
 })();
