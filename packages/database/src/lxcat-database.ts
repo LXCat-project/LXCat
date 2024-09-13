97cf656968b7162639dc2650771746c2bdf40715
Daan Boer <daan-boer@hotmail.com>
2024-09-13 13:54:22 +0200

    Correctly tie together different versions of the same item/set

--- packages/database/src/lxcat-database.ts
+++ packages/database/src/lxcat-database.ts
@@ -60,6 +60,7 @@ import {
 } from "./cs/queries/write.js";
 import {
   createHistoricDraftItem,
+  createHistoricDraftSet,
   createHistoricItem,
   loadHistoricDataset,
   updateHistoricItem,
@@ -388,6 +389,7 @@ export class LXCatDatabase {
 
   // css/migration
   public loadHistoricDataset = loadHistoricDataset;
+  public createHistoricDraftSet = createHistoricDraftSet;
   public createHistoricItem = createHistoricItem;
   public createHistoricDraftItem = createHistoricDraftItem;
   public updateHistoricItem = updateHistoricItem;
