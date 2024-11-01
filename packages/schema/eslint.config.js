7a2104d021267a42609ab59f83162ba70d10e2ee
Daan Boer <daan-boer@hotmail.com>
2024-11-01 09:20:57 +0100

    Fix `schema` and `database` lint configs

--- packages/schema/eslint.config.js
+++ packages/schema/eslint.config.js
@@ -1,15 +1,9 @@
-import jslint from "@eslint/js"
+import jslint from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default [
   jslint.configs.recommended,
   ...tseslint.configs.recommended,
-  {
-    files: ["src/**/*.ts"],
-    languageOptions: { globals: globals.browser },
-  },
-  {
-    ignores: ["*.{js,ts}", "dist/**"],
-  }
+  { languageOptions: { globals: globals.browser } },
 ];
