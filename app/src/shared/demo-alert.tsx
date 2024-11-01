8b4503a7587a2c4074ec0bc3f580bb01d379ab0e
Daan Boer <39552979+daanboer@users.noreply.github.com>
2024-11-01 11:29:41 +0100

    Update demo alert message (#1462)
    * Highlight links in the demo alert
    
    * Add message for contributors

--- app/src/shared/demo-alert.tsx
+++ app/src/shared/demo-alert.tsx
@@ -6,6 +6,8 @@ import { Alert, Center } from "@mantine/core";
 import { IconInfoCircle } from "@tabler/icons-react";
 import Link from "next/link";
 
+import classes from "./demo-alert.module.css";
+
 export const DemoAlert = () => (
   <Center>
     <Alert
@@ -15,22 +17,35 @@ export const DemoAlert = () => (
       color="red"
       style={{ width: "90%", marginTop: 10 }}
     >
-      This is the next version of the LXCat web site. The purpose of this demo
-      is to gather feedback on the interfaces, data format, and general
-      usability of the new website. Please communicate such feedback in the form
-      of issues on the{" "}
-      <Link href="https://github.com/LXCat-project/LXCat" target="_blank">
+      This is the next version of the LXCat website. The purpose of this demo is
+      to gather feedback on the interfaces, data format, and general usability
+      of the new website. Please communicate such feedback in the form of issues
+      on the{" "}
+      <Link
+        className={classes.link}
+        href="https://github.com/LXCat-project/LXCat"
+        target="_blank"
+      >
         LXCat GitHub page
       </Link>{" "}
       or in the form of an e-mail to{" "}
-      <Link href="mailto:info@lxcat.net">info@lxcat.net</Link>.{" "}
+      <Link className={classes.link} href="mailto:info@lxcat.net">
+        info@lxcat.net
+      </Link>.{" "}
+      Likewise, existing and new contributors that want to display their data on
+      LXCat 3 can reach out to the LXCat team by using the aforementioned links.
+      {" "}
       <b>
-        Do not use data downloaded from this deployment in publications. Until
-        the platform is officially released, unique identifiers of datasets and
-        cross sections will be unstable.
+        Do not use data downloaded from this deployment in publications yet.
+        Until the platform is officially released, unique identifiers of
+        datasets and cross sections will be unstable.
       </b>{" "}
       For now, please obtain your cross section data from{" "}
-      <Link href={"https://www.lxcat.net"} target="_blank">
+      <Link
+        className={classes.link}
+        href={"https://www.lxcat.net"}
+        target="_blank"
+      >
         the LXCat 2 website.
       </Link>
     </Alert>
