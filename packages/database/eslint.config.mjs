938417134b760c9a7f79cdbcd73c5d9404a2bc2c
Daan Boer <daan-boer@hotmail.com>
2024-11-01 09:22:21 +0100

    Fix REUSE compliance

--- packages/database/eslint.config.mjs
+++ packages/database/eslint.config.mjs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: LXCat team
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import jseslint from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
