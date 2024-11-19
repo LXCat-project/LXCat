3aa444131edf615a597b685510fabadc2f564915
Daan Boer <daan-boer@hotmail.com>
2024-11-19 14:21:52 +0100

    Add the `NewsCarousel` component
    This component will host all the `NewsCard` components and allow
    the user to click through them.

---
+++ app/src/news/carousel.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Carousel } from "@mantine/carousel";
+import { rem, useMantineTheme } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+import { NewsCard, NewsCardProps } from "./card";
+
+export type NewsCarouselProps = {
+  newsItems: Array<NewsCardProps>;
+};
+
+export function NewsCarousel({ newsItems }: NewsCarouselProps) {
+  const theme = useMantineTheme();
+  const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
+  const slides = newsItems.map((item) => (
+    <Carousel.Slide key={item.title}>
+      <NewsCard {...item} />
+    </Carousel.Slide>
+  ));
+
+  return (
+    <Carousel
+      slideSize={{ base: "100%", sm: "30%" }}
+      slideGap={{ base: "xl", sm: rem(5) }}
+      slidesToScroll={mobile ? 1 : 3}
+      align="center"
+    >
+      {slides}
+    </Carousel>
+  );
+}
