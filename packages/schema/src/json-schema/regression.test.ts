70c8c13897d3700418e5e0f91961286a2aed24aa
Daan Boer <daan-boer@hotmail.com>
2024-11-25 10:58:56 +0100

    Assert `type: json`

--- packages/schema/src/json-schema/regression.test.ts
+++ packages/schema/src/json-schema/regression.test.ts
@@ -3,7 +3,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { describe, expect, it } from "vitest";
-import OriginalLTPMixtureSchema from "../test-data/LTPMixture.schema.json";
+import OriginalLTPMixtureSchema from "../test-data/LTPMixture.schema.json" assert {
+  type: "json",
+};
 import { LTPMixtureJSONSchema } from "./mixture.js";
 
 describe("JSON Schema creation regression tests", () => {
