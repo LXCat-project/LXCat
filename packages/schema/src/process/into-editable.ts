ed1d42c6bce6d8263a3d32fe92278cc67ce07cc9
Daan Boer <daan-boer@hotmail.com>
2025-01-10 12:48:15 +0100

    Add helper function to transform `VersionedLTPDocument` into `EditedLTPDocument`

---
+++ packages/schema/src/process/into-editable.ts
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: LXCat team
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { EditedLTPDocument } from "../edited-document.js";
+import { VersionedLTPDocument } from "../versioned-document.js";
+
+export const intoEditable = (doc: VersionedLTPDocument): EditedLTPDocument => {
+  const editable: any = doc;
+
+  editable.contributor = doc.contributor.name;
+
+  return editable as EditedLTPDocument;
+};
