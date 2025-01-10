c83abe70cc9444232935168ed03917f79f59b4e2
Daan Boer <daan-boer@hotmail.com>
2025-01-10 14:57:15 +0100

    Fix linting error

--- packages/schema/src/process/into-editable.ts
+++ packages/schema/src/process/into-editable.ts
@@ -6,7 +6,7 @@ import { EditedLTPDocument } from "../edited-document.js";
 import { VersionedLTPDocument } from "../versioned-document.js";
 
 export const intoEditable = (doc: VersionedLTPDocument): EditedLTPDocument => {
-  const editable: any = doc;
+  const editable: Record<string, unknown> = doc;
 
   editable.contributor = doc.contributor.name;
 
