ba2b220723dafefb24207569a416abd19eb1dacf
Daan Boer <daan-boer@hotmail.com>
2025-01-15 17:24:42 +0100

    Update the `author/set/[id]/editraw` page
    Move the page to the app router.

---
+++ app/src/app/author/set/[id]/editraw/client-queries.tsx
@@ -0,0 +1,46 @@
+// SPDX-FileCopyrightText: LXCat team
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import Result, { err, ok } from "true-myth/result";
+import { ZodError } from "zod";
+
+export const uploadCS = async (
+  key: string,
+  docString: string,
+  commitMessage: string,
+): Promise<Result<string, Array<string>>> => {
+  const url = `/api/author/scat-css/${key}`;
+  const headers = new Headers({
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  });
+
+  let doc;
+
+  try {
+    doc = JSON.parse(docString);
+  } catch {
+    return err(["Invalid JSON."]);
+  }
+
+  const body = JSON.stringify({ doc, message: commitMessage });
+  const init = { method: "POST", body, headers };
+  const res = await fetch(url, init);
+  const data = await res.json();
+
+  if (res.ok) {
+    return ok(data.id);
+  }
+
+  if (data.issues) {
+    // Assume error is ZodError
+    return err(
+      (data as ZodError).issues.map((issue) =>
+        `${issue.path.slice(2).join("/")}: ${issue.message}`
+      ),
+    );
+  } else {
+    return err(data);
+  }
+};
---
+++ app/src/app/author/set/[id]/editraw/page.tsx
@@ -0,0 +1,59 @@
+// SPDX-FileCopyrightText: LXCat team
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import { options } from "@/auth/options";
+import { userIsAuthor } from "@/auth/page-guards";
+import { Forbidden } from "@/shared/forbidden";
+import { NotFound } from "@/shared/not-found";
+import { Unauthorized } from "@/shared/unauthorized";
+import { db } from "@lxcat/database";
+import { intoEditable } from "@lxcat/schema/process";
+import { getServerSession } from "next-auth/next";
+import { object, promise, string, TypeOf } from "zod";
+import { EditRawSetClient } from "./client-page";
+
+const ArangoKey = string().regex(/\d+/);
+const URLParams = object({
+  params: promise(object({ id: ArangoKey })),
+});
+
+type URLParams = TypeOf<typeof URLParams>;
+
+export default async function EditRawSetPage(props: URLParams) {
+  const { id } = await URLParams.parse(props).params;
+  const session = await getServerSession(options);
+
+  if (!userIsAuthor(session)) {
+    return <Unauthorized />;
+  }
+
+  if (!await userOwnsSet(session.user.email, id)) {
+    return <Forbidden />;
+  }
+
+  const set = await db().getSetById(id, true);
+
+  if (!set) return <NotFound />;
+
+  const versionInfo = await db().getSetVersionInfo(id);
+
+  return (
+    <EditRawSetClient
+      set={intoEditable(set)}
+      setKey={set._key}
+      commitMessage={versionInfo?.commitMessage ?? ""}
+    />
+  );
+}
+
+const userOwnsSet = async (email: string, setId: string): Promise<boolean> => {
+  const userAffiliations = await db().getAffiliations(email);
+  const setAffiliation = await db().getSetAffiliation(setId);
+
+  if (setAffiliation === undefined) {
+    return false;
+  }
+
+  return userAffiliations.map(({ name }) => name).includes(setAffiliation);
+};
