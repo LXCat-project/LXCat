52d7ff2704bbaae6de9168a085a58091edc86286
Daan Boer <daan-boer@hotmail.com>
2025-02-19 14:00:25 +0100

    First try at implementing species types

--- packages/schema/src/typebox.ts
+++ packages/schema/src/typebox.ts
@@ -1,4 +1,5 @@
-import { TObject, TSchema, Type } from "@sinclair/typebox";
+import { Static, TObject, TSchema, Type } from "@sinclair/typebox";
+import { serializeLatexLS, serializeLS } from "./species/atoms/ls.js";
 
 export const TReactionEntry = <StateType extends TSchema>(
   StateType: StateType,
@@ -148,3 +149,91 @@ export const EnergyRateCoefficientInfo = <ReferenceType extends TSchema>(
       threshold: UnitValue(Type.String(), Type.Number()),
     }),
   ]);
+
+type Serializable = {
+  summary: () => string;
+  latex: () => string;
+};
+
+export type Component<ComponentSchema extends TSchema> =
+  & Static<ComponentSchema>
+  & Serializable;
+
+export const makeComponent = <ComponentSchema extends TSchema>(
+  schema: ComponentSchema,
+  summary: (object: Static<ComponentSchema>) => string,
+  latex: (object: Static<ComponentSchema>) => string,
+) =>
+  Type.Unsafe<Component<ComponentSchema>>({
+    ...schema,
+    summary: () => summary(schema),
+    latex: () => latex(schema),
+  });
+
+export const LSTermUncoupled = Type.Object({
+  L: Type.Integer({ minimum: 0 }),
+  S: Type.Number({ minimum: 0, multipleOf: 0.5 }),
+  P: Type.Union([Type.Literal(-1), Type.Literal(1)]),
+});
+
+export const TotalAngularSpecifier = Type.Object({
+  J: Type.Number({ minimum: 0, multipleOf: 0.5 }),
+});
+
+export const LSTerm = Type.Composite([LSTermUncoupled, TotalAngularSpecifier]);
+
+export const buildTerm = <
+  EConfig extends TSchema,
+  TermSymbol extends TSchema,
+>(electronConfig: EConfig, term: TermSymbol) =>
+  Type.Object({ config: electronConfig, term });
+
+export const ShellEntry = Type.Object({
+  n: Type.Integer({ minimum: 1 }),
+  l: Type.Integer({ minimum: 0 }),
+  occupance: Type.Integer({ minimum: 0 }),
+});
+
+export const LSDescriptor = buildTerm(Type.Array(ShellEntry), LSTerm);
+
+export const LSComponent = makeComponent(
+  LSDescriptor,
+  serializeLS,
+  serializeLatexLS,
+);
+
+export const typeTag = <Tag extends string>(tag: Tag) =>
+  Type.Object({ type: Type.Literal(tag) });
+
+export const makeAtomSchema = <
+  Tag extends string,
+  CompositionSchema extends TObject,
+  ElectronicSchema extends TSchema,
+>(
+  tag: Tag,
+  composition: CompositionSchema,
+  electronic: ElectronicSchema,
+) =>
+  Type.Composite([
+    typeTag(tag),
+    composition,
+    Type.Object({
+      electronic: Type.Union([
+        { ...electronic, description: "Singular" },
+        Type.Array(electronic, { minimum: 2, description: "Compound" }),
+      ]),
+    }),
+  ]);
+
+export const makeAtom = <
+  Tag extends string,
+  CompositionSchema extends TObject,
+  ElectronicSchema extends TSchema,
+>(
+  tag: Tag,
+  composition: CompositionSchema,
+  electronic: Component<ElectronicSchema>,
+) => ({
+  plain: makeAtomSchema(tag, composition, electronic.innerType()),
+  serializable: makeAtomSchema(tag, composition, electronic),
+});
