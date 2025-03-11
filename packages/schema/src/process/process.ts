750f283d34183ca7823ea22cda7058060e8886a6
Daan Boer <daan-boer@hotmail.com>
2025-03-11 14:23:51 +0100

    Add typescript `Process` type

--- packages/schema/src/process/process.ts
+++ packages/schema/src/process/process.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { array, object, ZodTypeAny } from "zod";
+import { array, object, TypeOf, ZodType, ZodTypeAny } from "zod";
 import { Reaction } from "./reaction/index.js";
 
 export const Process = <
@@ -13,3 +13,9 @@ export const Process = <
     reaction: Reaction(StateType),
     info: array(ProcessInfoType),
   });
+
+type ProcessType<StateType extends ZodTypeAny, ProcessInfoType extends ZodTypeAny> = ReturnType<
+  typeof Process<StateType, ProcessInfoType>
+>;
+
+export type Process<StateType, ProcessInfoType> = TypeOf<ProcessType<ZodType<StateType>, ZodType<ProcessInfoType>>>;
