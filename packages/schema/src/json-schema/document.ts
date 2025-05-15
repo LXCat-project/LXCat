8904d446ae4bc56c15e847cc212e2cb4b6b4ca0e
Daan Boer <daan-boer@hotmail.com>
2025-05-15 13:44:56 +0200

    Further compatibility fixes for zod v4

--- packages/schema/src/json-schema/document.ts
+++ packages/schema/src/json-schema/document.ts
@@ -2,45 +2,51 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { zodToJsonSchema } from "zod-to-json-schema";
-import { CSLData } from "../common/csl/data.js";
-import { CSLDateVariable } from "../common/csl/date-variable.js";
-import { CSLNameVariable } from "../common/csl/name-variable.js";
-import { Key } from "../key.js";
+import z from "zod";
+// import { CSLData } from "../common/csl/data.js";
+// import { CSLDateVariable } from "../common/csl/date-variable.js";
+// import { CSLNameVariable } from "../common/csl/name-variable.js";
+// import { Key } from "../key.js";
 import { NewLTPDocument } from "../new-document.js";
-import { AnySpecies, Composition, Element } from "../species/index.js";
-import { VersionInfo } from "../version-info.js";
+// import { AnySpecies, Composition, Element } from "../species/index.js";
+// import { VersionInfo } from "../version-info.js";
 import { VersionedLTPDocument } from "../versioned-document.js";
 
-export const VersionedLTPDocumentJSONSchema = zodToJsonSchema(
+export const VersionedLTPDocumentJSONSchema = z.toJSONSchema(
   VersionedLTPDocument,
-  {
-    definitions: {
-      Key,
-      VersionInfo,
-      CSLData,
-      CSLNameVariable,
-      CSLDateVariable,
-      Element,
-      Composition,
-      AnySpecies,
-      ...Object.fromEntries(AnySpecies.optionsMap),
-    },
-    $refStrategy: "root",
-  },
 );
 
-export const NewLTPDocumentJSONSchema = zodToJsonSchema(NewLTPDocument, {
-  definitions: {
-    Key,
-    VersionInfo,
-    CSLData,
-    CSLNameVariable,
-    CSLDateVariable,
-    Element,
-    Composition,
-    AnySpecies,
-    ...Object.fromEntries(AnySpecies.optionsMap),
-  },
-  $refStrategy: "root",
-});
+// export const VersionedLTPDocumentJSONSchema = zodToJsonSchema(
+//   VersionedLTPDocument,
+//   {
+//     definitions: {
+//       Key,
+//       VersionInfo,
+//       CSLData,
+//       CSLNameVariable,
+//       CSLDateVariable,
+//       Element,
+//       Composition,
+//       AnySpecies,
+//       ...Object.fromEntries(AnySpecies.optionsMap),
+//     },
+//     $refStrategy: "root",
+//   },
+// );
+
+export const NewLTPDocumentJSONSchema = z.toJSONSchema(NewLTPDocument);
+
+// export const NewLTPDocumentJSONSchema = zodToJsonSchema(NewLTPDocument, {
+//   definitions: {
+//     Key,
+//     VersionInfo,
+//     CSLData,
+//     CSLNameVariable,
+//     CSLDateVariable,
+//     Element,
+//     Composition,
+//     AnySpecies,
+//     ...Object.fromEntries(AnySpecies.optionsMap),
+//   },
+//   $refStrategy: "root",
+// });
--- packages/schema/src/json-schema/mixture.ts
+++ packages/schema/src/json-schema/mixture.ts
@@ -2,28 +2,27 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { toJSONSchema } from "zod";
-import { zodToJsonSchema } from "zod-to-json-schema";
-import { CSLData } from "../common/csl/data.js";
-import { CSLDateVariable } from "../common/csl/date-variable.js";
-import { CSLNameVariable } from "../common/csl/name-variable.js";
-import { Key } from "../key.js";
+import z from "zod";
+// import { CSLData } from "../common/csl/data.js";
+// import { CSLDateVariable } from "../common/csl/date-variable.js";
+// import { CSLNameVariable } from "../common/csl/name-variable.js";
+// import { Key } from "../key.js";
 import { LTPMixture } from "../mixture.js";
-import { AtomComposition } from "../species/composition/atom.js";
-import { HeteronuclearDiatomComposition } from "../species/composition/diatom/heteronuclear.js";
-import { HomonuclearCompositionDescriptor } from "../species/composition/diatom/homonuclear.js";
-import { Element } from "../species/composition/element.js";
-import { LTICComposition } from "../species/composition/triatom/ltic.js";
-import { Composition } from "../species/composition/universal.js";
-import {
-  AnySpecies,
-  SerializedSpecies,
-  StateSummary,
-  SummarizedComponent,
-} from "../species/index.js";
-import { VersionInfo } from "../version-info.js";
+// import { AtomComposition } from "../species/composition/atom.js";
+// import { HeteronuclearDiatomComposition } from "../species/composition/diatom/heteronuclear.js";
+// import { HomonuclearCompositionDescriptor } from "../species/composition/diatom/homonuclear.js";
+// import { Element } from "../species/composition/element.js";
+// import { LTICComposition } from "../species/composition/triatom/ltic.js";
+// import { Composition } from "../species/composition/universal.js";
+// import {
+//   AnySpecies,
+//   SerializedSpecies,
+//   StateSummary,
+//   SummarizedComponent,
+// } from "../species/index.js";
+// import { VersionInfo } from "../version-info.js";
 
-export const LTPMixtureJSONSchema = toJSONSchema(LTPMixture);
+export const LTPMixtureJSONSchema = z.toJSONSchema(LTPMixture);
 
 // export const LTPMixtureJSONSchema = zodToJsonSchema(LTPMixture, {
 //   definitions: {
