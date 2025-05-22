7f128e2a1302fe0eda2d30e444532d678940f58f
Daan Boer <daan-boer@hotmail.com>
2025-05-22 09:59:32 +0200

    Make `VersionedDocumentBody` generic over the species type

--- packages/schema/src/migration/migration-document.ts
+++ packages/schema/src/migration/migration-document.ts
@@ -1,9 +1,11 @@
 import { TypeOf } from "zod";
 import { Reference } from "../common/reference.js";
+import { AnySpecies } from "../species/any-species.js";
 import { VersionedDocumentBody } from "../versioned-document.js";
 import { DOIString } from "./doi-string.js";
 
 export const LXCatMigrationDocument = VersionedDocumentBody(
+  AnySpecies,
   Reference.or(DOIString),
 );
 export type LXCatMigrationDocument = TypeOf<typeof LXCatMigrationDocument>;
