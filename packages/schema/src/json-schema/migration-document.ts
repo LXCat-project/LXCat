205603b7d0060c8c97f7df3746b456713026f4dd
Daan Boer <daan-boer@hotmail.com>
2025-05-22 09:40:58 +0200

    Update migration document schema generation to zod 4

--- packages/schema/src/json-schema/migration-document.ts
+++ packages/schema/src/json-schema/migration-document.ts
@@ -2,26 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { zodToJsonSchema } from "zod-to-json-schema";
-import { CSLData } from "../common/csl/data.js";
-import { CSLDateVariable } from "../common/csl/date-variable.js";
-import { CSLNameVariable } from "../common/csl/name-variable.js";
-import { Key } from "../key.js";
+import z from "zod";
 import { LXCatMigrationDocument } from "../migration/migration-document.js";
-import { AnySpecies, Composition, Element } from "../species/index.js";
-import { VersionInfo } from "../version-info.js";
 
-export const LXCat2MigrationSchema = zodToJsonSchema(LXCatMigrationDocument, {
-  definitions: {
-    Key,
-    VersionInfo,
-    CSLData,
-    CSLNameVariable,
-    CSLDateVariable,
-    Element,
-    Composition,
-    AnySpecies,
-    ...Object.fromEntries(AnySpecies.optionsMap),
-  },
-  $refStrategy: "root",
-});
+export const LXCat2MigrationSchema = z.toJSONSchema(LXCatMigrationDocument);
