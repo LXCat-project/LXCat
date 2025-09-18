ca6ea549f7bc21b523e50a77d1051525ded626d7
Daan Boer <daan-boer@hotmail.com>
2025-09-18 17:10:34 +0200

    Force the `/api-doc` page to be dynamic
    Next would build the `/api-doc` page as a static page,
    but the api endpoint URLs rely on `NEXT_PUBLIC_URL`,
    which is not generally available at build time. This
    is fixed by forcing Next to force the `/api-doc` to
    be dynamically rendered (on demand). In terms of
    performance this is not a big hit, as the OpenAPI
    spec is only generated once and then cached for
    reuse.

--- app/src/app/api-doc/page.tsx
+++ app/src/app/api-doc/page.tsx
@@ -3,18 +3,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import { generateOpenAPI } from "@/docs/openapi";
-// import { openapiGenerator } from "@/openapi";
 import { DocsPageClient } from "./page-client";
 
 export default async function DocsPage() {
   const spec = await generateOpenAPI();
-  // const spec = openapiGenerator.generate({
-  //   info: {
-  //     version: "0.0.1",
-  //     title: "LXCat API",
-  //     description: "API for working with LXCat data.",
-  //   },
-  //   servers: [{ url: "" }],
-  // });
   return <DocsPageClient spec={spec} />;
 }
+
+export const dynamic = "force-dynamic";
