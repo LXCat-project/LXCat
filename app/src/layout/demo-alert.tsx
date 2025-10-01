a799733fde0bf1f1141fbd3d9f1f8ccba3b2e8bd
Daan Boer <daan-boer@hotmail.com>
2025-10-01 14:10:58 +0200

    Update the alert for ChemCat

--- app/src/layout/demo-alert.tsx
+++ app/src/layout/demo-alert.tsx
@@ -12,15 +12,14 @@ export const DemoAlert = () => (
   <Center>
     <Alert
       icon={<IconInfoCircle />}
-      title="Welcome to the demo deployment of LXCat 3!"
+      title="Welcome to the demo deployment of ChemCat!"
       variant="filled"
       color="red"
       style={{ width: "90%", marginTop: 10 }}
     >
-      This is the next version of the LXCat website. The purpose of this demo is
-      to gather feedback on the interfaces, data format, and general usability
-      of the new website. Please communicate such feedback in the form of issues
-      on the{" "}
+      This is the prototype deployment of ChemCat. ChemCat is a data platform
+      for sharing plasma chemistry data. The purpose of this prototype is to
+      test and showcase ChemCat. The code is available on the{" "}
       <Link
         className={classes.link}
         href="https://github.com/LXCat-project/LXCat"
@@ -28,26 +27,11 @@ export const DemoAlert = () => (
       >
         LXCat GitHub page
       </Link>{" "}
-      or in the form of an e-mail to{" "}
-      <Link className={classes.link} href="mailto:info@lxcat.net">
-        info@lxcat.net
-      </Link>.{" "}
-      Likewise, existing and new contributors that want to display their data on
-      LXCat 3 can reach out to the LXCat team by using the aforementioned links.
-      {" "}
       <b>
         Do not use data downloaded from this deployment in publications yet.
         Until the platform is officially released, unique identifiers of
-        datasets and cross sections will be unstable.
-      </b>{" "}
-      For now, please obtain your cross section data from{" "}
-      <Link
-        className={classes.link}
-        href={"https://www.lxcat.net"}
-        target="_blank"
-      >
-        the LXCat 2 website.
-      </Link>
+        datasets and data items will be unstable.
+      </b>
     </Alert>
   </Center>
 );
