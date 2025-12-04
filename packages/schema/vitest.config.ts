354546cbc413c30e3064b7f08b5b05e54640b06b
Daan Boer <39552979+daanboer@users.noreply.github.com>
2025-12-04 08:47:59 +0100

    Only run tests from `schema` source directory (#2478)
    The current vitest config would also run tests in the dist directory. This is currently biting us in trying to merge #2467.

--- packages/schema/vitest.config.ts
+++ packages/schema/vitest.config.ts
@@ -6,10 +6,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    include: ["src/**/*.{spec,test}.ts"],
     coverage: {
       reporter: ["text", "json-summary", "json", "html"],
       include: ["src/**/*.ts"],
-      all: true,
     },
   },
 });
