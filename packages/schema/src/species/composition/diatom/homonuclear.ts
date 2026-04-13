5e5ffd241c045e4cb1e1d3e8ab3e61a313d7faef
Daan Boer <daan-boer@hotmail.com>
2026-04-13 10:54:43 +0200

    Support isotopes in homonuclear diatoms
    This specifically adds support for the HD, HT, and DT molecules.

--- packages/schema/src/species/composition/diatom/homonuclear.ts
+++ packages/schema/src/species/composition/diatom/homonuclear.ts
@@ -7,4 +7,9 @@ import { Element } from "../element.js";
 
 export const HomonuclearCompositionDescriptor = tuple([
   tuple([Element, literal(2)]),
-]);
+]).or(
+  tuple([
+    tuple([literal("H").or(literal("D")).or(literal("T")), literal(1)]),
+    tuple([literal("H").or(literal("D")).or(literal("T")), literal(1)]),
+  ]),
+);
