4a02acae33ef7a130067d5ae5e792bfcb28649d3
Daan Boer <39552979+daanboer@users.noreply.github.com>
2026-08-20 11:48:21 +0200

    Update theme and header colors (#3242)

--- app/src/theme.ts
+++ app/src/theme.ts
@@ -4,35 +4,22 @@
 
 "use client";
 
-import { createTheme, defaultVariantColorsResolver } from "@mantine/core";
+import { createTheme, MantineColorsTuple } from "@mantine/core";
+
+const brand: MantineColorsTuple = [
+  "#eaf5ff",
+  "#dae6f7",
+  "#b7cae5",
+  "#90acd4",
+  "#6e91c4",
+  "#5b82bc",
+  "#4f7ab9",
+  "#3f69a4",
+  "#345d94",
+  "#255084",
+];
 
 export const theme = createTheme({
-  colors: {
-    brand: [
-      "#e5f2ff",
-      "#c1d5f1",
-      "#9cb9e2",
-      "#769dd5",
-      "#5181c8",
-      "#3867af",
-      "#2a5089",
-      "#1c3963",
-      "#0e223e",
-      "#010b1b",
-    ],
-  },
+  colors: { brand },
   primaryColor: "brand",
-  variantColorResolver: (input) => {
-    const defaultColors = defaultVariantColorsResolver(input);
-
-    if (input.variant === "light") {
-      return {
-        ...defaultColors,
-        background: "var(--mantine-color-brand-0)",
-        hover: "var(--mantine-color-brand-1)",
-      };
-    }
-
-    return defaultColors;
-  },
 });
